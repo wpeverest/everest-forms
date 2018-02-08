0e5a8385c20fa613d924d2a4c2183a79a901d77b
Shiva Poudel <dev.shivapoudel@gmail.com>
2018-02-08 17:27:43 +0545

    Inline validation method

--- assets/js/frontend/everest-forms.js
+++ assets/js/frontend/everest-forms.js
@@ -14,7 +14,7 @@ jQuery( function ( $ ) {
 			this.load_validation();
 
 			// Inline validation
-			this.$everest_form.on( 'input validate change', '.input-text, select, input:checkbox', this.validate_field );
+			// this.$everest_form.on( 'input validate change', '.input-text, select, input:checkbox', this.validate_field );
 		},
 		init_datepicker: function () {
 			$( '.date-picker-field, .date-picker' ).datepicker({
@@ -28,8 +28,80 @@ jQuery( function ( $ ) {
 			});
 		},
 		load_validation: function() {
+			if ( typeof $.fn.validate === 'undefined' ) {
+				return false;
+			}
+
+			// Validate email addresses.
+			$.validator.methods.email = function( value, element ) {
+				/* https://stackoverflow.com/questions/2855865/jquery-validate-e-mail-address-regex */
+				var pattern = new RegExp(/^((([a-z]|\d|[!#\$%&'\*\+\-\/=\?\^_`{\|}~]|[\u00A0-\uD7FF\uF900-\uFDCF\uFDF0-\uFFEF])+(\.([a-z]|\d|[!#\$%&'\*\+\-\/=\?\^_`{\|}~]|[\u00A0-\uD7FF\uF900-\uFDCF\uFDF0-\uFFEF])+)*)|((\x22)((((\x20|\x09)*(\x0d\x0a))?(\x20|\x09)+)?(([\x01-\x08\x0b\x0c\x0e-\x1f\x7f]|\x21|[\x23-\x5b]|[\x5d-\x7e]|[\u00A0-\uD7FF\uF900-\uFDCF\uFDF0-\uFFEF])|(\\([\x01-\x09\x0b\x0c\x0d-\x7f]|[\u00A0-\uD7FF\uF900-\uFDCF\uFDF0-\uFFEF]))))*(((\x20|\x09)*(\x0d\x0a))?(\x20|\x09)+)?(\x22)))@((([a-z]|\d|[\u00A0-\uD7FF\uF900-\uFDCF\uFDF0-\uFFEF])|(([a-z]|\d|[\u00A0-\uD7FF\uF900-\uFDCF\uFDF0-\uFFEF])([a-z]|\d|-|\.|_|~|[\u00A0-\uD7FF\uF900-\uFDCF\uFDF0-\uFFEF])*([a-z]|\d|[\u00A0-\uD7FF\uF900-\uFDCF\uFDF0-\uFFEF])))\.)+(([a-z]|[\u00A0-\uD7FF\uF900-\uFDCF\uFDF0-\uFFEF])|(([a-z]|[\u00A0-\uD7FF\uF900-\uFDCF\uFDF0-\uFFEF])([a-z]|\d|-|\.|_|~|[\u00A0-\uD7FF\uF900-\uFDCF\uFDF0-\uFFEF])*([a-z]|[\u00A0-\uD7FF\uF900-\uFDCF\uFDF0-\uFFEF])))\.?$/i);
+				return this.optional( element ) || pattern.test( value );
+			};
+
 			this.$everest_form.each( function() {
-				$( this ).validate();
+				var $this = $( this );
+
+				$this.validate({
+					rules: {
+						name: "required",
+						email: {
+							required: true,
+							email: true
+						}
+					},
+					messages: {
+						name: "Please specify your name",
+						email: {
+							required: "We need your email address to contact you",
+							email: "Your email address must be in the format of name@domain.com"
+						}
+					},
+					errorClass: 'evf-error',
+					validClass: 'evf-valid',
+					errorPlacement: function( error, element ) {
+						window.console.log( error );
+
+						if ( 'radio' === element.attr( 'type' ) || 'checkbox' === element.attr( 'type' ) ) {
+							element.parent().parent().parent().append( error );
+						} else if ( element.is( 'select' ) && element.attr( 'class' ).match( /date-month|date-day|date-year/ ) ) {
+							if ( element.parent().find( 'label.evf-error:visible' ).length === 0 ) {
+								element.parent().find( 'select:last' ).after( error );
+							}
+						} else {
+							error.insertAfter( element );
+						}
+					},
+					highlight: function( element, errorClass, validClass ) {
+						var $element  = $( element ),
+							$parent   = $element.closest( '.form-row' ),
+							inputName = $element.attr( 'name' );
+
+						if ( $element.attr( 'type' ) === 'radio' || $element.attr( 'type' ) === 'checkbox' ) {
+							$parent.find( 'input[name=\''+inputName+'\']' ).addClass( errorClass ).removeClass( validClass );
+						} else {
+							$element.addClass( errorClass ).removeClass( validClass );
+						}
+
+						$parent.addClass( 'evf-has-error' );
+					},
+					unhighlight: function( element, errorClass, validClass ) {
+						var $element  = $( element ),
+							$parent   = $element.closest( '.form-row' ),
+							inputName = $element.attr( 'name' );
+
+						if ( $element.attr( 'type' ) === 'radio' || $element.attr( 'type' ) === 'checkbox' ) {
+							$parent.find( 'input[name=\''+inputName+'\']' ).addClass( validClass ).removeClass( errorClass );
+						} else {
+							$element.addClass( validClass ).removeClass( errorClass );
+						}
+
+						$parent.removeClass( 'evf-has-error' );
+					},
+					submitHandler: function( form ) {
+						form.submit();
+					}
+				});
 			});
 		},
 		validate_field: function ( e ) {
