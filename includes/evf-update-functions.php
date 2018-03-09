3a661cf6cfd6917f76aa6cb7726dadfb86ff19dd
sanjeev3 <sanjeev.themegrill@gmail.com>
2018-03-09 17:27:46 +0545

    Migration script for forms meta-key

--- includes/evf-update-functions.php
+++ includes/evf-update-functions.php
@@ -39,6 +39,29 @@ function evf_update_103_db_version() {
 }
 
 /**
+ * Update all forms for meta-key.
+ */
+function evf_update_110_update_forms() {
+	$forms = evf_get_all_forms();
+
+	foreach ( $forms as $form_id => $form ) {
+		$form_obj  = EVF()->form->get( $form_id );
+		$form_data = ! empty( $form_obj->post_content ) ? evf_decode( $form_obj->post_content ) : '';
+
+		if ( ! empty( $form_data['form_fields'] ) ) {
+			foreach ( $form_data['form_fields'] as &$form_fields ) {
+				if ( ! isset( $form_fields['meta-key'] ) ) {
+					$form_fields['meta-key'] = evf_get_meta_key_field_option( $form_fields );
+				}
+			}
+		}
+
+		// Update form data.
+		EVF()->form->update( $form_id, $form_data );
+	}
+}
+
+/**
  * Update DB Version.
  */
 function evf_update_110_db_version() {
