f1b0c54d9fff6f38972267c5009beaa31112d6aa
Shiva Poudel <dev.shivapoudel@gmail.com>
2018-03-20 12:02:39 +0545

    Addons search purify styles

--- includes/admin/views/html-admin-page-addons.php
+++ includes/admin/views/html-admin-page-addons.php
@@ -13,7 +13,7 @@ defined( 'ABSPATH' ) || exit;
 	<h1 class="wp-heading-inline"><?php _e( 'Everest Forms Add-ons', 'everest-forms' ); ?></h1>
 
 	<?php if ( apply_filters( 'everest_forms_refresh_addons', true ) ) : ?>
-		<a href="<?php echo esc_url( $refresh_url ); ?>" class="page-title-action"><span class="dashicons dashicons-image-rotate"></span> <?php esc_html_e( 'Refresh Addons', 'everest-forms' ); ?></a>
+		<a href="<?php echo esc_url( $refresh_url ); ?>" class="page-title-action"><span class="dashicons dashicons-image-rotate"></span> <?php esc_html_e( 'Refresh Add-ons', 'everest-forms' ); ?></a>
 	<?php endif; ?>
 
 	<hr class="wp-header-end">
@@ -22,19 +22,16 @@ defined( 'ABSPATH' ) || exit;
 	<?php if ( $sections ) : ?>
 		<ul class="subsubsub">
 			<?php foreach ( $sections as $section ) : ?>
-				<li>
-					<a class="<?php echo $current_section === $section->slug ? 'current' : ''; ?>" href="<?php echo admin_url( 'admin.php?page=evf-addons&section=' . esc_attr( $section->slug ) ); ?>">
+				<li class="<?php echo esc_attr( $section->slug ); ?>">
+					<a <?php echo $current_section === $section->slug ? 'class="current"' : ''; ?> href="<?php echo admin_url( 'admin.php?page=evf-addons&section=' . esc_attr( $section->slug ) ); ?>">
 						<?php echo esc_html( $section->label ); ?>
 					</a>
-					<?php echo ( end( $section_keys ) !== $section->slug ) ? ' |' : ''; ?>
 				</li>
 			<?php endforeach; ?>
 		</ul>
 
 		<?php if ( ! empty( $_GET['search'] ) ) : ?>
-			<h1 class="search-form-title" >
-				<?php printf( __( 'Showing search results for: %s', 'everest-forms' ), '<strong>' . esc_html( $_GET['search'] ) . '</strong>' ); ?>
-			</h1>
+			<h2 class="search-form-title" ><?php printf( __( 'Showing search results for: %s', 'everest-forms' ), '<strong>' . esc_html( $_GET['search'] ) . '</strong>' ); ?></h2>
 		<?php endif; ?>
 
 		<form class="search-form" method="GET">
@@ -47,6 +44,6 @@ defined( 'ABSPATH' ) || exit;
 			<input type="text" name="search" value="<?php echo esc_attr( isset( $_GET['search'] ) ? $_GET['search'] : '' ); ?>" placeholder="<?php _e( 'Enter a search term and press enter', 'everest-forms' ); ?>">
 		</form>
 	<?php else : ?>
-		<p><?php printf( __( 'Our catalog of WooCommerce Extensions can be found on WooCommerce.com here: <a href="%s">WooCommerce Extensions Catalog</a>', 'woocommerce' ), 'https://woocommerce.com/product-category/woocommerce-extensions/' ); ?></p>
+		<p><?php printf( __( 'Our catalog of Everest Forms Add-ons/Extensions can be found on WPEverest.com here: <a href="%s">Everest Forms Extensions Catalog</a>', 'everest-forms' ), 'https://wpeverest.com/wordpress-plugins/everest-forms/' ); ?></p>
 	<?php endif; ?>
 </div>
