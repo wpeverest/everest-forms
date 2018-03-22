b55a36d6fa74db3b27a7c316a74a6e5422ecaddb
Shiva Poudel <dev.shivapoudel@gmail.com>
2018-03-22 11:02:57 +0545

    Addon page styling

--- includes/admin/views/html-admin-page-addons.php
+++ includes/admin/views/html-admin-page-addons.php
@@ -4,33 +4,35 @@
  *
  * @var string $view
  * @var object $addons
+ *
+ * @package EverestForms
  */
 
 defined( 'ABSPATH' ) || exit;
 
 ?>
 <div class="wrap everest-forms evf_addons_wrap">
-	<h1 class="wp-heading-inline"><?php _e( 'Everest Forms Add-ons', 'everest-forms' ); ?></h1>
+	<h1 class="wp-heading-inline"><?php esc_html_e( 'Everest Forms Add-ons', 'everest-forms' ); ?></h1>
 
 	<?php if ( apply_filters( 'everest_forms_refresh_addons', true ) ) : ?>
 		<a href="<?php echo esc_url( $refresh_url ); ?>" class="page-title-action"><?php esc_html_e( 'Refresh Add-ons', 'everest-forms' ); ?></a>
 	<?php endif; ?>
 
 	<hr class="wp-header-end">
-	<h2 class="screen-reader-text"><?php _e( 'Filter Everest Forms Extensions', 'everest-forms' ); ?></h1>
+	<h2 class="screen-reader-text"><?php esc_html_e( 'Filter Everest Forms Extensions', 'everest-forms' ); ?></h1>
 
 	<?php if ( $sections ) : ?>
 		<ul class="subsubsub">
 			<?php foreach ( $sections as $section ) : ?>
 				<li class="<?php echo esc_attr( $section->slug ); ?>">
-					<a href="<?php echo admin_url( 'admin.php?page=evf-addons&section=' . esc_attr( $section->slug ) ); ?>"<?php echo $current_section === $section->slug ? ' class="current" aria-current="page"' : ''; ?>><?php echo esc_html( $section->label ); ?></a>
+					<a href="<?php echo esc_url( admin_url( 'admin.php?page=evf-addons&section=' . esc_attr( $section->slug ) ) ); ?>"<?php echo $current_section === $section->slug ? ' class="current" aria-current="page"' : ''; ?>><?php echo esc_html( $section->label ); ?></a>
 					<?php echo ( end( $section_keys ) !== $section->slug ) ? ' |' : ''; ?>
 				</li>
 			<?php endforeach; ?>
 		</ul>
 
 		<?php if ( ! empty( $_GET['search'] ) ) : ?>
-			<h2 class="search-form-title" ><?php printf( __( 'Showing search results for: %s', 'everest-forms' ), '<strong>' . esc_html( $_GET['search'] ) . '</strong>' ); ?></h2>
+			<h2 class="search-form-title" ><?php printf( esc_html__( 'Showing search results for: %s', 'everest-forms' ), '<strong>' . esc_html( $_GET['search'] ) . '</strong>' ); ?></h2>
 		<?php endif; ?>
 
 		<form class="search-form hidden" method="GET">
@@ -43,15 +45,25 @@ defined( 'ABSPATH' ) || exit;
 			<input type="text" name="search" value="<?php echo esc_attr( isset( $_GET['search'] ) ? $_GET['search'] : '' ); ?>" placeholder="<?php _e( 'Enter a search term and press enter', 'everest-forms' ); ?>">
 		</form>
 		<?php if ( '_featured' !== $current_section && $addons ) : ?>
-			<div class="addons-list">
+			<div class="addons-container">
 			<?php foreach ( $addons as $addon ) : ?>
 				<div class="addon-container">
-					<div class="details">
-						<img src="<?php echo esc_url( $addon->image ); ?>">
-						<h5 class="addon-name"><?php echo esc_html( $addon->title ); ?></h5>
-						<p class="addon-desc"><?php echo esc_html( $addon->excerpt ); ?></p>
+					<div class="addon-item">
+						<div class="details">
+							<img src="<?php echo esc_url( $addon->image ); ?>">
+							<h5 class="addon-name"><?php echo esc_html( $addon->title ); ?></h5>
+							<p class="addon-desc"><?php echo esc_html( $addon->excerpt ); ?></p>
+						</div>
+						<div class="actions">
+							<div class="status">
+								<strong>Status:</strong>
+								<span class="status-label status-download">Not Installed</span>
+							</div>
+							<div class="action-button">
+								<button class="button button-secondary install-now">Install Addon</button>
+							</div>
+						</div>
 					</div>
-					<div class="actions"></div>
 				</div>
 			<?php endforeach; ?>
 			</div>
