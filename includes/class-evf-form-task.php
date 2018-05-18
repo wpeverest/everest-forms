e6a84eb9b0e79d6d729a22793f9fe9630ad111f3
Sanzeeb Aryal <sanzeeb.aryal@gmail.com>
2018-05-18 12:16:05 +0545

    Filter hook conflict fix

--- includes/class-evf-form-task.php
+++ includes/class-evf-form-task.php
@@ -348,7 +348,7 @@ class EVF_Form_Task {
 		$emails->__set( 'from_name', $email['sender_name'] );
 		$emails->__set( 'from_address', $email['sender_address'] );
 		$emails->__set( 'reply_to', isset( $email['user_email'] ) ? $email['user_email'] : $email['sender_address'] );
-		$emails->__set( 'attachments', apply_filters( 'everest_forms_email_attachments', $attachment, $entry, $form_data ) );
+		$emails->__set( 'attachments', apply_filters( 'everest_forms_email_file_attachments', $attachment, $entry, $form_data ) );
 
 		// Send entry email.
 		foreach ( $email['address'] as $address ) {
