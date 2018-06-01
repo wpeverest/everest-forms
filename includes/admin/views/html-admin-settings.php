4360309cb3cf622e3ff0fe4027b541e81c014534
Shiva Poudel <dev.shivapoudel@gmail.com>
2018-06-01 13:56:50 +0545

    Fix - WP notices class not displaying properly

--- includes/admin/views/html-admin-settings.php
+++ includes/admin/views/html-admin-settings.php
@@ -20,25 +20,27 @@ if ( ! $tab_exists ) {
 		<nav class="nav-tab-wrapper evf-nav-tab-wrapper">
 			<?php
 			foreach ( $tabs as $slug => $tab ) {
-				echo '<a href="' . esc_html( admin_url( 'admin.php?page=evf-settings&tab=' . esc_attr( $slug ) ) ) . '" class="nav-tab ' . ( $current_tab === $slug ? 'nav-tab-active' : '' ) . '" data-key="' . esc_attr( $slug ) . '"><span class="' . esc_attr( $tab['icon'] ) . '"></span>' . esc_html( $tab['label'] ) . '</a>';
+				echo '<a href="' . esc_html( admin_url( 'admin.php?page=evf-settings&tab=' . esc_attr( $slug ) ) ) . '" class="nav-tab ' . ( $current_tab === $slug ? 'nav-tab-active' : '' ) . '"><span class="' . esc_attr( $tab['icon'] ) . '"></span>' . esc_html( $tab['label'] ) . '</a>';
 			}
 
 			do_action( 'everest_forms_settings_tabs' );
 			?>
 		</nav>
 		<h1 class="screen-reader-text"><?php echo esc_html( $current_tab_label['label'] ); ?></h1>
-		<?php
-			do_action( 'everest_forms_sections_' . $current_tab );
+		<div class="everest-forms-settings">
+			<?php
+				do_action( 'everest_forms_sections_' . $current_tab );
 
-			self::show_messages();
+				self::show_messages();
 
-			do_action( 'everest_forms_settings_' . $current_tab );
-		?>
-		<p class="submit">
-			<?php if ( empty( $GLOBALS['hide_save_button'] ) ) : ?>
-				<button name="save" class="button-primary everest-forms-save-button" type="submit" value="<?php esc_attr_e( 'Save Changes', 'everest-forms' ); ?>"><?php esc_html_e( 'Save Changes', 'everest-forms' ); ?></button>
-			<?php endif; ?>
-			<?php wp_nonce_field( 'everest-forms-settings' ); ?>
-		</p>
+				do_action( 'everest_forms_settings_' . $current_tab );
+			?>
+			<p class="submit">
+				<?php if ( empty( $GLOBALS['hide_save_button'] ) ) : ?>
+					<button name="save" class="button-primary everest-forms-save-button" type="submit" value="<?php esc_attr_e( 'Save Changes', 'everest-forms' ); ?>"><?php esc_html_e( 'Save Changes', 'everest-forms' ); ?></button>
+				<?php endif; ?>
+				<?php wp_nonce_field( 'everest-forms-settings' ); ?>
+			</p>
+		</div>
 	</form>
 </div>
