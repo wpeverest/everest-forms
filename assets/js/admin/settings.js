923f3917f37f2530aeedd2f9028cbaf3de4f3c71
Shiva Poudel <dev.shivapoudel@gmail.com>
2018-06-01 12:16:10 +0545

    Reload and save action for each tab in settings

--- assets/js/admin/settings.js
+++ assets/js/admin/settings.js
@@ -1,5 +1,7 @@
-/* global jconfirm */
-(function ( $ ) {
+/* global everest_forms_settings_params, jconfirm */
+( function( $ ) {
+
+	// Confirm defaults.
 	$( document ).ready( function () {
 		// jquery-confirm defaults.
 		jconfirm.defaults = {
@@ -12,14 +14,75 @@
 			boxWidth: '400px',
 			columnClass: 'evf-responsive-class'
 		};
+	});
 
-		$( 'body' ).on( 'click', '.evf-nav-tab-wrapper a.nav-tab', function( event) {
-			event.preventDefault();
-			var tab = $( this ).attr( 'data-key' );
-			$( '.everest-forms .nav-tab' ).removeClass( 'nav-tab-active' );
-			$( this ).addClass( 'nav-tab-active' );
-			$( '.evf-setting-tab-content' ).removeClass( 'active' );
-			$( '.evf-setting-tab-content[data-content-key="' + tab + '"]' ).addClass( 'active' );
+	// Color picker
+	$( '.colorpick' )
+
+		.iris({
+			change: function( event, ui ) {
+				$( this ).parent().find( '.colorpickpreview' ).css({ backgroundColor: ui.color.toString() });
+			},
+			hide: true,
+			border: true
+		})
+
+		.on( 'click focus', function( event ) {
+			event.stopPropagation();
+			$( '.iris-picker' ).hide();
+			$( this ).closest( 'td' ).find( '.iris-picker' ).show();
+			$( this ).data( 'original-value', $( this ).val() );
+		})
+
+		.on( 'change', function() {
+			if ( $( this ).is( '.iris-error' ) ) {
+				var original_value = $( this ).data( 'original-value' );
+
+				if ( original_value.match( /^\#([a-fA-F0-9]{6}|[a-fA-F0-9]{3})$/ ) ) {
+					$( this ).val( $( this ).data( 'original-value' ) ).change();
+				} else {
+					$( this ).val( '' ).change();
+				}
+			}
 		});
+
+	$( 'body' ).on( 'click', function() {
+		$( '.iris-picker' ).hide();
+	});
+
+	// Edit prompt
+	$( function() {
+		var changed = false;
+
+		$( 'input, textarea, select, checkbox' ).change( function() {
+			changed = true;
+		});
+
+		$( '.evf-nav-tab-wrapper a' ).click( function() {
+			if ( changed ) {
+				window.onbeforeunload = function() {
+					return everest_forms_settings_params.i18n_nav_warning;
+				};
+			} else {
+				window.onbeforeunload = '';
+			}
+		});
+
+		$( '.submit input' ).click( function() {
+			window.onbeforeunload = '';
+		});
+	});
+
+	// Select all/none
+	$( '.everest-forms' ).on( 'click', '.select_all', function() {
+		$( this ).closest( 'td' ).find( 'select option' ).attr( 'selected', 'selected' );
+		$( this ).closest( 'td' ).find( 'select' ).trigger( 'change' );
+		return false;
+	});
+
+	$( '.everest-forms' ).on( 'click', '.select_none', function() {
+		$( this ).closest( 'td' ).find( 'select option' ).removeAttr( 'selected' );
+		$( this ).closest( 'td' ).find( 'select' ).trigger( 'change' );
+		return false;
 	});
 })( jQuery );
--- includes/admin/views/html-admin-settings.php
+++ includes/admin/views/html-admin-settings.php
@@ -19,11 +19,11 @@ if ( ! $tab_exists ) {
 	<form method="<?php echo esc_attr( apply_filters( 'everest_forms_settings_form_method_tab_' . $current_tab, 'post' ) ); ?>" id="mainform" action="" enctype="multipart/form-data">
 		<nav class="nav-tab-wrapper evf-nav-tab-wrapper">
 			<?php
-				foreach ( $tabs as $slug => $tab ) {
-					echo '<a href="' . esc_html( admin_url( 'admin.php?page=evf-settings&tab=' . esc_attr( $slug ) ) ) . '" class="nav-tab ' . ( $current_tab === $slug ? 'nav-tab-active' : '' ) . '" data-key="' . esc_attr( $slug ) . '"><span class="' . esc_attr( $tab['icon'] ) . '"></span>' . esc_html( $tab['label'] ) . '</a>';
-				}
+			foreach ( $tabs as $slug => $tab ) {
+				echo '<a href="' . esc_html( admin_url( 'admin.php?page=evf-settings&tab=' . esc_attr( $slug ) ) ) . '" class="nav-tab ' . ( $current_tab === $slug ? 'nav-tab-active' : '' ) . '" data-key="' . esc_attr( $slug ) . '"><span class="' . esc_attr( $tab['icon'] ) . '"></span>' . esc_html( $tab['label'] ) . '</a>';
+			}
 
-				do_action( 'everest_forms_settings_tabs' );
+			do_action( 'everest_forms_settings_tabs' );
 			?>
 		</nav>
 		<h1 class="screen-reader-text"><?php echo esc_html( $current_tab_label['label'] ); ?></h1>
@@ -32,17 +32,11 @@ if ( ! $tab_exists ) {
 
 			self::show_messages();
 
-			foreach ( $tabs as $tab_key => $tab_data ) :
-				?>
-				<div data-content-key="<?php echo $tab_key; ?>" class="evf-setting-tab-content <?php echo( $current_tab == $tab_key ? 'active' : '' ); ?>">
-					<?php do_action( 'everest_forms_settings_' . $tab_key ); ?>
-				</div>
-				<?php
-			endforeach;
+			do_action( 'everest_forms_settings_' . $current_tab );
 		?>
 		<p class="submit">
 			<?php if ( empty( $GLOBALS['hide_save_button'] ) ) : ?>
-				<button name="save" class="button-primary everest-forms-save-button" type="submit" value="<?php esc_attr_e( 'Save changes', 'everest-forms' ); ?>"><?php esc_html_e( 'Save changes', 'everest-forms' ); ?></button>
+				<button name="save" class="button-primary everest-forms-save-button" type="submit" value="<?php esc_attr_e( 'Save Changes', 'everest-forms' ); ?>"><?php esc_html_e( 'Save Changes', 'everest-forms' ); ?></button>
 			<?php endif; ?>
 			<?php wp_nonce_field( 'everest-forms-settings' ); ?>
 		</p>
