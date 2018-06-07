09819de7bf10c3f467c966564ebe15f1bff1254f
Roshan Sameer <belong2roshan@gmail.com>
2018-06-07 16:42:33 +0545

    Fix - active class after deleting field

--- assets/js/admin/everest-panel-builder.js
+++ assets/js/admin/everest-panel-builder.js
@@ -606,6 +606,9 @@
 									$field.fadeOut( 'slow', function () {
 										$field.remove();
 										option_field.remove();
+										$( '.everest-forms-fields-tab' ).find( 'a' ).removeClass( 'active' );
+										$( '.everest-forms-fields-tab' ).find( 'a' ).first().addClass( 'active' );
+										$( '.everest-forms-add-fields' ).show();
 									});
 									if( grid.children().length === 1 ) {
 										grid.addClass( 'evf-empty-grid' );
