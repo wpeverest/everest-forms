6553c4882847e79f483b71653faa600ec4f05542
Shiva Poudel <dev.shivapoudel@gmail.com>
2018-07-03 16:56:14 +0545

    No need

--- includes/class-evf-forms-feature.php
+++ includes/class-evf-forms-feature.php
@@ -24,7 +24,6 @@ class EVF_Forms_Features {
 	 */
 	public function __construct() {
 		add_action( 'everest_forms_form_settings_notifications', array( $this, 'form_settings_notifications' ), 8, 1 );
-		// add_filter( 'everest_forms_builder_fields_groups', array( $this, 'fields_groups' ), 20 );
 
 		if ( ! defined( 'EFP_PLUGIN_FILE' ) ) {
 			add_filter( 'everest_forms_fields', array( $this, 'form_fields' ) );
