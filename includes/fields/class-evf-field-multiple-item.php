06770ed8241e0fbf113d6c64d6c25dca00f668cd
Roshan Sameer <belong2roshan@gmail.com>
2018-07-03 17:35:03 +0545

    Pro Field - Payment added by autoloader way

---
+++ includes/fields/class-evf-field-multiple-item.php
@@ -0,0 +1,27 @@
+<?php
+/**
+ * Multiple Item field
+ *
+ * @package EverestForms\Fields
+ * @since   1.2.0
+ */
+
+defined( 'ABSPATH' ) || exit;
+
+/**
+ * EVF_Field_Multiple_Item Class.
+ */
+class EVF_Field_Multiple_Item extends EVF_Form_Fields {
+
+	/**
+	 * Constructor.
+	 */
+	public function init() {
+		$this->name     = esc_html__( 'Multiple Items', 'everest-forms-pro' );
+		$this->type     = 'payment-multiple';
+		$this->icon     = 'evf-icon evf-icon-multiple-choices';
+		$this->order    = 20;
+		$this->group    = 'payment';
+		$this->is_pro = true;
+	}
+}
---
+++ includes/fields/class-evf-field-total.php
@@ -0,0 +1,27 @@
+<?php
+/**
+ * Total field
+ *
+ * @package EverestForms\Fields
+ * @since   1.2.0
+ */
+
+defined( 'ABSPATH' ) || exit;
+
+/**
+ * EVF_Field_Total Class.
+ */
+class EVF_Field_Total extends EVF_Form_Fields {
+
+	/**
+	 * Constructor.
+	 */
+	public function init() {
+		$this->name  = esc_html__( 'Total', 'everest-forms-pro' );
+		$this->type  = 'payment-total';
+		$this->icon  = 'evf-icon evf-icon-total';
+		$this->order = 21;
+		$this->group = 'payment';
+		$this->is_pro = true;
+	}
+}
