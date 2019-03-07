703bc5e0d7ec8f2fa769be555848eaeb420ad322
Ujwol Bastakoti <bktujwol@gmail.com>
2019-03-07 12:30:42 +0545

    added signature file

---
+++ includes/fields/class-evf-field-signature.php
@@ -0,0 +1,29 @@
+<?php
+/**
+ *  Signature field.
+ *
+ * @package EverestForms\Fields
+ * @since   1.5.0
+ */
+
+defined( 'ABSPATH' ) || exit;
+
+/**
+ * EVF_Field_Signature Class.
+ */
+class EVF_Field_Signature extends EVF_Form_Fields {
+
+	/**
+	 * Constructor.
+	 */
+	public function __construct() {
+		$this->name   = esc_html__( 'e-Signature', 'everest-forms' );
+		$this->type   = 'signature';
+		$this->icon   = 'evf-icon evf-icon-img-upload';
+		$this->order  = 90;
+		$this->group  = 'advanced';
+		$this->is_pro = true;
+
+		parent::__construct();
+	}
+}
