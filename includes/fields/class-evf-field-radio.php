34ab08d6fce1e02590404d2549b3a874630af7c7
Roshan Sameer <belong2roshan@gmail.com>
2019-03-11 10:15:51 +0545

    Fix - radio buttons and checkboxes fix on label click

--- includes/fields/class-evf-field-radio.php
+++ includes/fields/class-evf-field-radio.php
@@ -40,7 +40,7 @@ class EVF_Field_Radio extends EVF_Form_Fields {
 			),
 		);
 		$this->settings = array(
-			'basic-options' => array(
+			'basic-options'    => array(
 				'field_options' => array(
 					'label',
 					'meta',
@@ -81,10 +81,14 @@ class EVF_Field_Radio extends EVF_Form_Fields {
 				),
 				false
 			);
-			$this->field_element( 'row', $field, array(
-				'slug'    => 'show_values',
-				'content' => $show_values,
-			) );
+			$this->field_element(
+				'row',
+				$field,
+				array(
+					'slug'    => 'show_values',
+					'content' => $show_values,
+				)
+			);
 		}
 	}
 
@@ -145,16 +149,16 @@ class EVF_Field_Radio extends EVF_Form_Fields {
 	 * @param array $form_data
 	 */
 	public function field_display( $field, $field_atts, $form_data ) {
- 		// Setup and sanitize the necessary data
-		$field          = apply_filters( 'everest_forms_radio_field_display', $field, $field_atts, $form_data );
-		$conditional_id     = isset( $field['properties']['inputs']['primary']['attr']['conditional_id'] ) ? $field['properties']['inputs']['primary']['attr']['conditional_id'] : '';
-		$conditional_rules  = isset( $field['properties']['inputs']['primary']['attr']['conditional_rules'] ) ? $field['properties']['inputs']['primary']['attr']['conditional_rules'] : '';
-		$field_required = ! empty( $field['required'] ) ? ' required' : '';
-		$field_class    = implode( ' ', array_map( 'sanitize_html_class', $field_atts['input_class'] ) );
-		$field_id       = implode( ' ', array_map( 'sanitize_html_class', $field_atts['input_id'] ) );
-		$field_data     = '';
-		$form_id        = $form_data['id'];
-		$choices        = isset( $field['choices'] ) ? $field['choices'] : array();
+		// Setup and sanitize the necessary data
+		$field             = apply_filters( 'everest_forms_radio_field_display', $field, $field_atts, $form_data );
+		$conditional_id    = isset( $field['properties']['inputs']['primary']['attr']['conditional_id'] ) ? $field['properties']['inputs']['primary']['attr']['conditional_id'] : '';
+		$conditional_rules = isset( $field['properties']['inputs']['primary']['attr']['conditional_rules'] ) ? $field['properties']['inputs']['primary']['attr']['conditional_rules'] : '';
+		$field_required    = ! empty( $field['required'] ) ? ' required' : '';
+		$field_class       = implode( ' ', array_map( 'sanitize_html_class', $field_atts['input_class'] ) );
+		$field_id          = implode( ' ', array_map( 'sanitize_html_class', $field_atts['input_id'] ) );
+		$field_data        = '';
+		$form_id           = $form_data['id'];
+		$choices           = isset( $field['choices'] ) ? $field['choices'] : array();
 		if ( ! empty( $field_atts['input_data'] ) ) {
 			foreach ( $field_atts['input_data'] as $key => $val ) {
 				$field_data .= ' data-' . $key . '="' . $val . '"';
@@ -172,7 +176,8 @@ class EVF_Field_Radio extends EVF_Form_Fields {
 			printf( '<li class="choice-%d depth-%d">', $key, $depth );
 
 			// Checkbox elements
-			printf( "<input type='radio' class='input-text' id='evf-%d-field_%s_%d' name='everest_forms[form_fields][%s][]' value='%s' %s %s conditional_rules='%s' conditional_id='%s'>",
+			printf(
+				"<input type='radio' class='input-text' id='everest-forms-%d-field_%s_%d' name='everest_forms[form_fields][%s][]' value='%s' %s %s conditional_rules='%s' conditional_id='%s'>",
 				$form_id,
 				$field['id'],
 				$key,
@@ -184,7 +189,7 @@ class EVF_Field_Radio extends EVF_Form_Fields {
 				$conditional_id
 			);
 
-			printf( '<label class="everest-forms-field-label-inline" for="everest-forms-%d-field_%d_%d">%s</label>', $form_id, $field['id'], $key, wp_kses_post( $choice['label'] ) );
+			printf( '<label class="everest-forms-field-label-inline" for="everest-forms-%d-field_%s_%d">%s</label>', $form_id, $field['id'], $key, wp_kses_post( $choice['label'] ) );
 
 			echo '</li>';
 		}
@@ -259,7 +264,6 @@ class EVF_Field_Radio extends EVF_Form_Fields {
 		} else {
 
 			// Normal processing, dynamic population is off
-
 			// If show_values is true, that means values posted are the raw values
 			// and not the labels. So we need to get the label values.
 			if ( ! empty( $field['show_values'] ) && '1' == $field['show_values'] ) {
