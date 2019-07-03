d666ab3b9ffac0b4486ab7f65bf58de049b9eaa8
Shiva Poudel <dev.shivapoudel@gmail.com>
2019-07-03 14:55:55 +0545

    Fix - Email field should default to being required

--- includes/fields/class-evf-field-email.php
+++ includes/fields/class-evf-field-email.php
@@ -23,7 +23,7 @@ class EVF_Field_Email extends EVF_Form_Fields {
 		$this->order    = 90;
 		$this->group    = 'general';
 		$this->settings = array(
-			'basic-options' => array(
+			'basic-options'    => array(
 				'field_options' => array(
 					'label',
 					'meta',
@@ -46,31 +46,49 @@ class EVF_Field_Email extends EVF_Form_Fields {
 	}
 
 	/**
+	 * Hook in tabs.
+	 */
+	public function init_hooks() {
+		add_filter( 'everest_forms_field_new_required', array( $this, 'field_default_required' ), 5, 3 );
+	}
+
+	/**
+	 * Field should default to being required.
+	 *
+	 * @since 1.4.10
+	 *
+	 * @param bool  $required Required status, true is required.
+	 * @param array $field    Field settings.
+	 *
+	 * @return bool
+	 */
+	public function field_default_required( $required, $field ) {
+		if ( 'email' === $field['type'] ) {
+			return true;
+		}
+
+		return $required;
+	}
+
+	/**
 	 * Field preview inside the builder.
 	 *
-	 * @since      1.0.0
+	 * @since 1.0.0
 	 *
-	 * @param array $field
+	 * @param array $field    Field settings.
 	 */
 	public function field_preview( $field ) {
-
-		// Define data.
-		$placeholder         = ! empty( $field['placeholder'] ) ? esc_attr( $field['placeholder'] ) : '';
-		$confirm_placeholder = ! empty( $field['confirmation_placeholder'] ) ? esc_attr( $field['confirmation_placeholder'] ) : '';
-		$confirm             = ! empty( $field['confirmation'] ) ? 'enabled' : 'disabled';
+		$placeholder = ! empty( $field['placeholder'] ) ? esc_attr( $field['placeholder'] ) : '';
+		$confirm     = ! empty( $field['confirmation'] ) ? 'enabled' : 'disabled';
 
 		// Label.
 		$this->field_preview_option( 'label', $field );
 		?>
-
 		<div class="everest-forms-confirm everest-forms-confirm-<?php echo $confirm; ?>">
-
 			<div class="everest-forms-confirm-primary">
 				<input type="email" placeholder="<?php echo $placeholder; ?>" class="widefat" disabled>
 			</div>
-
 		</div>
-
 		<?php
 		// Description.
 		$this->field_preview_option( 'description', $field );
@@ -81,9 +99,9 @@ class EVF_Field_Email extends EVF_Form_Fields {
 	 *
 	 * @since      1.0.0
 	 *
-	 * @param array $field
-	 * @param array $deprecated
-	 * @param array $form_data
+	 * @param array $field      Field settings.
+	 * @param array $deprecated Deprecated array.
+	 * @param array $form_data  Form data and settings.
 	 */
 	public function field_display( $field, $deprecated, $form_data ) {
 
@@ -97,7 +115,8 @@ class EVF_Field_Email extends EVF_Form_Fields {
 		if ( ! $confirmation ) {
 
 			// Primary field.
-			printf( '<input type="email" %s %s>',
+			printf(
+				'<input type="email" %s %s>',
 				evf_html_attributes( $primary['id'], $primary['class'], $primary['data'], $primary['attr'] ),
 				$primary['required']
 			);
@@ -111,7 +130,8 @@ class EVF_Field_Email extends EVF_Form_Fields {
 			// Primary field.
 			echo '<div ' . evf_html_attributes( false, $primary['block'] ) . '>';
 			$this->field_display_sublabel( 'primary', 'before', $field );
-			printf( '<input type="email" %s %s>',
+			printf(
+				'<input type="email" %s %s>',
 				evf_html_attributes( $primary['id'], $primary['class'], $primary['data'], $primary['attr'] ),
 				$primary['required']
 			);
@@ -122,7 +142,8 @@ class EVF_Field_Email extends EVF_Form_Fields {
 			// Secondary field.
 			echo '<div ' . evf_html_attributes( false, $secondary['block'] ) . '>';
 			$this->field_display_sublabel( 'secondary', 'before', $field );
-			printf( '<input type="email" %s %s>',
+			printf(
+				'<input type="email" %s %s>',
 				evf_html_attributes( $secondary['id'], $secondary['class'], $secondary['data'], $secondary['attr'] ),
 				$secondary['required']
 			);
@@ -132,20 +153,18 @@ class EVF_Field_Email extends EVF_Form_Fields {
 
 			echo '</div>';
 
-		} // End if().
+		}
 	}
 
 	/**
 	 * Formats and sanitizes field.
 	 *
-	 * @param int    $field_id
-	 * @param array  $field_submit
-	 * @param array  $form_data
-	 * @param string $meta_key
+	 * @param int    $field_id     Field ID.
+	 * @param array  $field_submit Submitted field value.
+	 * @param array  $form_data    Form data and settings.
+	 * @param string $meta_key     Field meta key.
 	 */
 	public function format( $field_id, $field_submit, $form_data, $meta_key ) {
-
-		// Define data.
 		if ( is_array( $field_submit ) ) {
 			$value = ! empty( $field_submit['primary'] ) ? $field_submit['primary'] : '';
 		} else {
