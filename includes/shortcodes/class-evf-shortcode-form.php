cf38ac64a25cab62245cf177633821d7aa3c7c09
Shiva Poudel <dev.shivapoudel@gmail.com>
2019-09-02 16:35:10 +0545

    Revert all the bw compat code as new way is needed

--- includes/shortcodes/class-evf-shortcode-form.php
+++ includes/shortcodes/class-evf-shortcode-form.php
@@ -660,11 +660,7 @@ class EVF_Shortcode_Form {
 		}
 
 		// Allow Multi-Part to be customized.
-		if ( isset( self::$parts[ $form_id ] ) ) {
-			self::$parts[ $form_id ] = apply_filters( 'everest_forms_parts_data', self::$parts[ $form_id ], $form_data, $form_id );
-		} else {
-			self::$parts = apply_filters( 'everest_forms_parts_data', self::$parts, $form_data, $form_id );
-		}
+		self::$parts[ $form_id ] = apply_filters( 'everest_forms_parts_data', self::$parts, $form_data, $form_id );
 
 		// Allow final action to be customized.
 		$action = apply_filters( 'everest_forms_frontend_form_action', $action, $form_data );
