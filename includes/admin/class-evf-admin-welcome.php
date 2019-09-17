31de5f36fc6df333c499ee6e03bc8a68eacd6682
Nilan Bhasima <nilanbhas@gmail.com>
2019-09-17 11:46:31 +0545

    Welcome page header

--- includes/admin/class-evf-admin-welcome.php
+++ includes/admin/class-evf-admin-welcome.php
@@ -43,13 +43,20 @@ class EVF_Admin_Welcome {
 	public function welcome_page() {
 		?>
 		<div id="everest-forms-welcome" >
-			<div class="welcome-header">
-				<img src=""/>
-				<span><?php esc_html_e( 'Getting Started', 'everest-forms' ); ?></span>
+			<div class="eveverest-forms-welcome-header">
+				<div class="eveverest-forms-welcome-header__logo-wrap">
+					<div class="eveverest-forms-welcome-header__logo-icon">
+					<svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 24 24"><path d="M18.88,3l1.37,2.25H15.89L14.52,3ZM21,21H1L12,3.15l6.84,11.11H10.6L12,12H14.8L12,7.43,5,18.74H21.58L23,21ZM18.64,9.77,17.27,7.53h4.36L23,9.77Z"/></svg>
+					</div>
+					<span><?php esc_html_e( 'Getting Started', 'everest-forms' ); ?></span>
+				</div>
+				<a class="everest-forms-welcome__skip" href="#">
+					<span class="dashicons dashicons-no-alt"></span>
+				</a>
 			</div>
 			<div class="welcome-container">
 				<div class="welcome-title-description">
-					<h4><?php esc_html_e( 'Welcome to Everest Forms', 'everest-forms' ); ?></h4>
+					<h2><?php esc_html_e( 'Welcome to Everest Forms', 'everest-forms' ); ?></h2>
 					<span class="description"><?php esc_html_e( 'Thank you for choosing Everest Forms, the most poweful and easy drag & drop WordPress form builder in the market.', 'everest-forms' ); ?></span>
 				</div>
 				<div class="welcome-video">
