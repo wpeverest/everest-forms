90fb01e619ed12816cae72c28fc427be3fc13ac8
Deepak Sharma <sharmadpk03@gmail.com>
2019-09-18 12:14:17 +0545

    Fix - Undefined label for html field translation

--- includes/class-evf-ajax.php
+++ includes/class-evf-ajax.php
@@ -218,8 +218,10 @@ class EVF_AJAX {
 		$empty_meta_data = array();
 		if ( ! empty( $data['form_fields'] ) ) {
 			foreach ( $data['form_fields'] as $field ) {
-				// register string for translation.
-				evf_string_translation( $data['id'], $field['id'], $field['label'] );
+				// Register string for translation.
+				if ( isset( $field['label'] ) ) {
+					evf_string_translation( $data['id'], $field['id'], $field['label'] );
+				}
 
 				if ( empty( $field['meta-key'] ) && ! in_array( $field['type'], array( 'html', 'title', 'captcha' ), true ) ) {
 					$empty_meta_data[] = $field['label'];
