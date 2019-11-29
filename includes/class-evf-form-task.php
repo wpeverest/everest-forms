60e5e12b57d8b7a6b5397c87f8907cdb35f83cbd
unknown <iamprazol@gmail.com>
2019-11-29 17:30:58 +0545

    code alignments maintained

--- includes/class-evf-form-task.php
+++ includes/class-evf-form-task.php
@@ -442,11 +442,11 @@ class EVF_Form_Task {
 			$emails->__set( 'from_name', $email['sender_name'] );
 			$emails->__set( 'from_address', $email['sender_address'] );
 			$emails->__set( 'reply_to', $email['reply_to'] );
-			if($entry_id) {
-				$emails->__set('attachments', apply_filters('everest_forms_email_file_attachments', $attachment, $entry, $form_data, 'entry-email', $connection_id, $entry_id));
-			} else {
-				return;
+
+			if( $entry_id ) {
+				$emails->__set( 'attachments', apply_filters('everest_forms_email_file_attachments', $attachment, $entry, $form_data, 'entry-email', $connection_id, $entry_id ));
 			}
+
 			// Maybe include Cc and Bcc email addresses.
 			if ( 'yes' === get_option( 'everest_forms_enable_email_copies' ) ) {
 				if ( ! empty( $notification['evf_carboncopy'] ) ) {
