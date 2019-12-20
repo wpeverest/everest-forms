2b0c46af2869aa9477c6d447d8b0b0b0815a305b
Nilan Bhasima <nilanbhas@gmail.com>
2019-12-20 12:40:31 +0545

    Import forms card design

--- includes/admin/views/html-admin-page-import.php
+++ includes/admin/views/html-admin-page-import.php
@@ -8,13 +8,17 @@
 defined( 'ABSPATH' ) || exit;
 
 ?>
-<div class="everest-forms-import-form">
+<div class="everest-forms-import-form everest-forms-sm-card">
 	<form method="post" enctype="multipart/form-data" action="<?php echo esc_url( admin_url( 'admin.php?page=evf-tools&tab=import' ) ); ?>">
 		<h3><?php echo __( 'Import Everest Forms', 'everest-forms' ); ?></h3>
-		<p><?php echo __( 'Import CSV file.', 'everest-forms' ); ?></p>
-		<input type="file" name="file" id="everest-forms-import" <?php esc_attr_e( 'files selected', 'everest-forms' ); ?>" accept=".json" />
-		<div class="publishing-action"></div>
-			<button type="submit" class="everest-forms-btn everest-forms-btn-primary everest_forms_import_action" name="everest-forms-import-form"><?php esc_html_e( 'Import', 'everest-forms' ); ?></button>
+		<p><?php echo __( 'Select CSV file to import the form.', 'everest-forms' ); ?></p>
+		<div class="everest-forms-file-upload">
+			<input type="file" name="file" id="everest-forms-import" <?php esc_attr_e( 'files selected', 'everest-forms' ); ?>" accept=".json" />
+			<label for="everest-forms-import"><span class="everest-forms-btn dashicons dashicons-upload">Choose File</span>No file selected</label>
+		</div>
+		<p class="description"><i class="dashicons dashicons-info"></i><?php echo __( 'Only ' . '<strong>' . 'JSON' . '</strong>' . ' file is allowed.', 'everest-forms' ); ?></p>
+		<div class="publishing-action">
+			<button type="submit" class="everest-forms-btn everest-forms-btn-primary everest_forms_import_action" name="everest-forms-import-form"><?php esc_html_e( 'Import Form', 'everest-forms' ); ?></button>
 			<?php wp_nonce_field( 'everest_forms_import_nonce', 'everest-forms-import-nonce' ); ?>
 		</div>
 	</form>
