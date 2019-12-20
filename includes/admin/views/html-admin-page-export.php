edce0704c54e4f91118c269a2bfcd070da45a3b4
roshansameer <belong2roshan@gmail.com>
2019-12-20 15:03:45 +0545

    Import Export Completed

--- includes/admin/views/html-admin-page-export.php
+++ includes/admin/views/html-admin-page-export.php
@@ -8,21 +8,25 @@
 defined( 'ABSPATH' ) || exit;
 
 ?>
-<form method="post" action="<?php echo esc_url( admin_url( 'admin.php?page=evf-tools&tab=export' ) ); ?>">
+<div class="everest-forms-import-form everest-forms-sm-card">
+	<h3><?php echo __( 'Export Everest Forms with Settings', 'everest-forms' ); ?></h3>
+	<p><?php echo __( 'Export your forms along with their settings as JSON file.', 'everest-forms' ); ?></p>
+	<div class="everest-forms-file-upload">
 	<?php
 	$forms   = evf_get_all_forms( true );
 	if ( ! empty( $forms ) ) {
-			echo '<select id="everest-forms-form-export" style="min-width: 350px;" name="form_id" data-placeholder="' . esc_attr__( 'Select form', 'everest-forms' ) . '">';
+			echo '<select id="everest-forms-form-export" style="min-width: 350px;" name="form_id" data-placeholder="' . esc_attr__( 'Select form', 'everest-forms' ) . '"><option value="">'.__( 'Select a form', 'everest-forms').'</option>';
 			foreach ( $forms as $id => $form ) :
 				echo '<option value="'.esc_attr( $id ).'">'.esc_html( $form ).'</option>';
 			endforeach;
 			echo '</select>';
-		echo '</span>';
 	} else {
 		echo '<p>' . esc_html__( 'You need to create a form before you can use form export.', 'everest-forms' ) . '</p>';
 	}
 	?>
-	<br>
-	<?php wp_nonce_field( 'everest_forms_export_nonce', 'everest-forms-export-nonce' ); ?>
-	<button type="submit" name="everest-forms-export-form"><?php esc_html_e( 'Export', 'everest-forms' ); ?></button>
-</form>
+	</div>
+	<div class="publishing-action">
+		<?php wp_nonce_field( 'everest_forms_export_nonce', 'everest-forms-export-nonce' ); ?>
+		<button type="submit" class="everest-forms-btn everest-forms-btn-primary" name="everest-forms-export-form"><?php esc_html_e( 'Export', 'everest-forms' ); ?></button>
+	</div>
+</div>
--- includes/admin/views/html-admin-page-import.php
+++ includes/admin/views/html-admin-page-import.php
@@ -9,17 +9,15 @@ defined( 'ABSPATH' ) || exit;
 
 ?>
 <div class="everest-forms-import-form everest-forms-sm-card">
-	<form method="post" enctype="multipart/form-data" action="<?php echo esc_url( admin_url( 'admin.php?page=evf-tools&tab=import' ) ); ?>">
-		<h3><?php echo __( 'Import Everest Forms', 'everest-forms' ); ?></h3>
-		<p><?php echo __( 'Select JSON file to import the form.', 'everest-forms' ); ?></p>
-		<div class="everest-forms-file-upload">
-			<input type="file" name="file" id="everest-forms-import" <?php esc_attr_e( 'files selected', 'everest-forms' ); ?>" accept=".json" />
-			<label for="everest-forms-import"><span class="everest-forms-btn dashicons dashicons-upload">Choose File</span><span id="import-file-name"><?php echo __('No file selected', 'everest-forms'); ?></span></label>
-		</div>
-		<p class="description"><i class="dashicons dashicons-info"></i><?php echo __( 'Only ' . '<strong>' . 'JSON' . '</strong>' . ' file is allowed.', 'everest-forms' ); ?></p>
-		<div class="publishing-action">
-			<button type="submit" class="everest-forms-btn everest-forms-btn-primary everest_forms_import_action" name="everest-forms-import-form"><?php esc_html_e( 'Import Form', 'everest-forms' ); ?></button>
-			<?php wp_nonce_field( 'everest_forms_import_nonce', 'everest-forms-import-nonce' ); ?>
-		</div>
-	</form>
+	<h3><?php echo __( 'Import Everest Forms', 'everest-forms' ); ?></h3>
+	<p><?php echo __( 'Select JSON file to import the form.', 'everest-forms' ); ?></p>
+	<div class="everest-forms-file-upload">
+		<input type="file" name="file" id="everest-forms-import" <?php esc_attr_e( 'files selected', 'everest-forms' ); ?>" accept=".json" />
+		<label for="everest-forms-import"><span class="everest-forms-btn dashicons dashicons-upload">Choose File</span><span id="import-file-name"><?php echo __('No file selected', 'everest-forms'); ?></span></label>
+	</div>
+	<p class="description"><i class="dashicons dashicons-info"></i><?php echo __( 'Only ' . '<strong>' . 'JSON' . '</strong>' . ' file is allowed.', 'everest-forms' ); ?></p>
+	<div class="publishing-action">
+		<button type="submit" class="everest-forms-btn everest-forms-btn-primary everest_forms_import_action" name="everest-forms-import-form"><?php esc_html_e( 'Import Form', 'everest-forms' ); ?></button>
+		<?php wp_nonce_field( 'everest_forms_import_nonce', 'everest-forms-import-nonce' ); ?>
+	</div>
 </div>
