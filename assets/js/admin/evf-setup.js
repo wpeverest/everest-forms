4f4e00acdfd4b06b95c4ff631895043f9bdf7425
roshansameer <belong2roshan@gmail.com>
2019-12-27 11:58:23 +0545

    Form name popup added

--- assets/js/admin/evf-setup.js
+++ assets/js/admin/evf-setup.js
@@ -23,45 +23,67 @@ jQuery( function( $ ) {
 		template_select: function( e ) {
 			e.preventDefault();
 			var $this        = $( this ),
-				// $formName    = $formName.val('test'),
 				template     = $this.data( 'template' ),
 				templateName = $this.data( 'template-name-raw' ),
-				formName     = '';
+				formName     = '',
+				namePrompt   = evf_setup_params.i18n_form_name,
+				nameField    = '<input autofocus="" type="text" id="everest-forms-setup-name" class="everest-forms-setup-name" placeholder="'+evf_setup_params.i18n_form_placeholder+'">',
+				nameError    = '<p class="error">'+evf_setup_params.i18n_form_error_name+'</p>',
+				modalContent = namePrompt+nameField+nameError;
+				$.confirm({
+					title: false,
+					content: modalContent,
+				   icon: 'dashicons dashicons-info',
+					type: 'blue',
+					backgroundDismiss: false,
+					closeIcon: false,
+					buttons: {
+						confirm: {
+							text: evf_setup_params.i18n_form_ok,
+							btnClass: 'btn-confirm',
+							keys: ['enter'],
+							action: function() {
+							// Don't do anything for selects that trigger modal.
+							if ( $this.parent().hasClass( 'loading' ) ) {
+								return;
+							}
+							var $formName    = $( '#everest-forms-setup-name' );
+							// Check that form title is provided.
+							if ( ! $formName.val() ) {
+								formName = templateName;
+								$( '.everest-forms-setup-name' ).addClass( 'everest-forms-required' ).focus();
+								return false;
+							} else {
+								formName = $formName.val();
+							}
 
-			// Don't do anything for selects that trigger modal.
-			if ( $this.parent().hasClass( 'loading' ) ) {
-				return;
-			}
+							$this.parent().addClass( 'loading' );
 
-			// Check that form title is provided.
-			// if ( ! $formName.val() ) {
-				formName = templateName;
-				// $( '.everest-forms-setup-name' ).addClass( 'everest-forms-required' ).focus();
-				// return false;
-			// } else {
-			// 	formName = $formName.val();
-			// }
+							var data = {
+								title: formName,
+								action: 'everest_forms_create_form',
+								template: template,
+								security: evf_setup_params.create_form_nonce
+							};
 
-			$this.parent().addClass( 'loading' );
-
-			var data = {
-				title: formName,
-				action: 'everest_forms_create_form',
-				template: template,
-				security: evf_setup_params.create_form_nonce
-			};
-
-			$.post( evf_setup_params.ajax_url, data, function( response ) {
-				if ( response.success ) {
-					$this.parent().removeClass( 'loading' );
-					window.location.href = response.data.redirect;
-				} else {
-					$this.parent().removeClass( 'loading' );
-					$( '.everest-forms-setup-name' ).addClass( 'everest-forms-required' ).focus();
-					window.console.log( response );
+							$.post( evf_setup_params.ajax_url, data, function( response ) {
+								if ( response.success ) {
+									$this.parent().removeClass( 'loading' );
+									window.location.href = response.data.redirect;
+								} else {
+									$this.parent().removeClass( 'loading' );
+									$( '.everest-forms-setup-name' ).addClass( 'everest-forms-required' ).focus();
+									window.console.log( response );
+								}
+							}).fail( function( xhr ) {
+								window.console.log( xhr.responseText );
+							});
+						}
+					},
+					cancel: {
+						text: evf_email_params.i18n_email_cancel
+					}
 				}
-			}).fail( function( xhr ) {
-				window.console.log( xhr.responseText );
 			});
 		},
 		input_keypress: function ( e ) {
--- includes/admin/class-evf-admin-assets.php
+++ includes/admin/class-evf-admin-assets.php
@@ -198,6 +198,10 @@ class EVF_Admin_Assets {
 					array(
 						'ajax_url'          => admin_url( 'admin-ajax.php' ),
 						'create_form_nonce' => wp_create_nonce( 'everest_forms_create_form' ),
+						'i18n_form_name'  => esc_html__( 'Enter a Form name', 'everest-forms' ),
+						'i18n_form_error_name'  => esc_html__( 'You must provide a Form name', 'everest-forms' ),
+						'i18n_form_ok'          => esc_html__( 'OK', 'everest-forms' ),
+						'i18n_form_placeholder' => esc_html__( 'Eg: Contact Form', 'everest-forms' ),
 					)
 				);
 			}
