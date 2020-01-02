6fdf9a5f658b719233a2ebbd2e33a85f83caea7b
Roshan Sameer <belong2roshan@gmail.com>
2020-01-02 15:27:50 +0545

    Feature - Import Export of Everest Forms (#321)
    * Import Export Menu and page added
    
    * Status menu renamed as Tools and import export tab added
    
    * Page added for import and export tab
    
    * Doc block added for import and export page
    
    * Export of Form
    
    * Tweak - import section
    
    * Form Import sucess
    
    * Updated From id on imported form
    
    * Extracted buttons styles
    
    * Import forms card design
    
    * spinner managed and file name managed
    
    * Import Export Completed
    
    * Default tab changed for tools page
    
    * Tweak - Import and Export card design
    
    * Import export ready for testing
    
    * Removed unwanted code
    
    * Development code removed
    
    * Text domain changed
    
    * Tweak
    
    * Error message changed
    
    * Tweak - Copy shortcode button design
    
    * Grunt task
    
    * Refactor: Code quality
    
    * In the status page do not load some scripts
    
    * Code inspection for #326
    
    * Code inspection for import-export feature #321
    
    * Logic tweaks
    
    * PHPCS and other code inspection
    
    * Safely land the status page to tools page
    
    * PHPCS
    
    Co-authored-by: Nilanzva <31344435+Nilanzva@users.noreply.github.com>
    Co-authored-by: sharmahub <37950847+sharmahub@users.noreply.github.com>
    Co-authored-by: Nobel Dahal <iamtribulation@outlook.com>
    Co-authored-by: Shiva Poudel <3774827+shivapoudel@users.noreply.github.com>

---
+++ includes/admin/views/html-admin-page-export.php
@@ -0,0 +1,32 @@
+<?php
+/**
+ * Admin View: Page - Export
+ *
+ * @package EverestForms/Admin/Export
+ */
+
+defined( 'ABSPATH' ) || exit;
+
+?>
+<div class="everest-forms-export-form">
+	<h3><?php echo __( 'Export Everest Forms with Settings', 'everest-forms' ); ?></h3>
+	<p><?php echo __( 'Export your forms along with their settings as JSON file.', 'everest-forms' ); ?></p>
+	<form method="post" action="<?php echo esc_url( admin_url( 'admin.php?page=evf-tools&tab=export' ) ); ?>">
+		<?php
+		$forms = evf_get_all_forms( true );
+		if ( ! empty( $forms ) ) {
+			echo '<select id="everest-forms-form-export" style="min-width: 350px;" name="form_id" data-placeholder="' . esc_attr__( 'Select form', 'everest-forms' ) . '"><option value="">' . __( 'Select a form', 'everest-forms' ) . '</option>';
+			foreach ( $forms as $id => $form ) { // phpcs:ignore WordPress.WP.GlobalVariablesOverride
+				echo '<option value="' . esc_attr( $id ) . '">' . esc_html( $form ) . '</option>';
+			}
+			echo '</select>';
+		} else {
+			echo '<p>' . esc_html__( 'You need to create a form before you can use form export.', 'everest-forms' ) . '</p>';
+		}
+		?>
+		<div class="publishing-action">
+			<?php wp_nonce_field( 'everest_forms_export_nonce', 'everest-forms-export-nonce' ); ?>
+			<button type="submit" class="everest-forms-btn everest-forms-btn-primary everest-forms-export-form-action" name="everest-forms-export-form"><?php esc_html_e( 'Export', 'everest-forms' ); ?></button>
+		</div>
+	</form>
+</div>
