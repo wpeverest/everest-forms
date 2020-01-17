0bdf603c5e5804060eb97b2692156e347012ede3
Shiva Poudel <dev.shivapoudel@gmail.com>
2020-01-17 17:29:43 +0545

    Test flow for entries db cols

--- includes/evf-core-functions.php
+++ includes/evf-core-functions.php
@@ -1962,3 +1962,12 @@ function evf_string_translation( $form_id, $field_id, $variable ) {
 
 	return $variable;
 }
+
+/**
+ * Add entries columns.
+ */
+function evf_update_160_add_entries_columns() {
+	global $wpdb;
+
+}
+add_action( 'init', 'evf_update_160_add_entries_columns' );
