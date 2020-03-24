a5e73152cad7e0218d6a7f6299122d707f12056a
Shiva Poudel <dev.shivapoudel@gmail.com>
2020-03-24 16:41:23 +0545

    Fix - Kses filter to only allow certain html within label

--- includes/abstracts/class-evf-form-fields.php
+++ includes/abstracts/class-evf-form-fields.php
@@ -1117,7 +1117,7 @@ abstract class EVF_Form_Fields {
 
 		switch ( $option ) {
 			case 'label':
-				$label  = isset( $field['label'] ) && ! empty( $field['label'] ) ? $field['label'] : '';
+				$label  = isset( $field['label'] ) && ! empty( $field['label'] ) ? wp_kses( $field['label'], 'post' ) : '';
 				$output = sprintf( '<label class="label-title %s"><span class="text">%s</span><span class="required">*</span></label>', $class, $label );
 				break;
 
