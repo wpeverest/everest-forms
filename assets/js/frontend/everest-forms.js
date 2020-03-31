5d7521aeb3102714badba7cbe0c333e79a0eb954
Krishna Suwal <58679177+krishna-suwal@users.noreply.github.com>
2020-03-31 14:19:33 +0545

    added local page support for privacy policy field

--- assets/js/frontend/everest-forms.js
+++ assets/js/frontend/everest-forms.js
@@ -17,9 +17,31 @@ jQuery( function ( $ ) {
 			this.load_validation();
 			this.submission_scroll();
 			this.randomize_elements();
+			this.init_privacy_policy_fields();
 
 			// Inline validation.
 			this.$everest_form.on( 'input validate change', '.input-text, select, input:checkbox, input:radio', this.validate_field );
+		},
+		init_privacy_policy_fields: function() {
+			$( document.body ).on( 'click', '.evf-privacy-policy-local-page-link', function ( e ) {
+				e.preventDefault();
+
+				var page_id = $( this ).data( 'page-id' );
+
+				if ( '' !== page_id ) {
+					var $field = $( this ).closest( '.evf-field' );
+
+					if ( $( this ).hasClass( 'evf-page-expanded' ) ) {
+						$( this ).removeClass( 'evf-page-expanded' );
+						$field.find( '.evf-privacy-policy-local-page-content-' + page_id ).slideUp();
+					} else {
+						$field.find( '.evf-privacy-policy-local-page-content:visible' ).slideUp();
+						$field.find( '.evf-privacy-policy-local-page-content-' + page_id ).slideDown();
+						$field.find( '.evf-privacy-policy-local-page-link' ).removeClass( 'evf-page-expanded' );
+						$( this ).addClass( 'evf-page-expanded' );
+					}
+				}
+			});
 		},
 		init_inputMask: function() {
 			// Only load if jQuery inputMask library exists.
