3096ac3156fea6e09a90a9d0eb6a90c91a122303
Krishna Suwal <58679177+krishna-suwal@users.noreply.github.com>
2020-05-12 18:11:48 +0545

    Tweak - changed field priority

--- includes/fields/class-evf-field-range-slider.php
+++ includes/fields/class-evf-field-range-slider.php
@@ -20,7 +20,7 @@ class EVF_Field_Range_Slider extends EVF_Form_Fields {
 		$this->name   = esc_html__( 'Range Slider', 'everest-forms' );
 		$this->type   = 'range-slider';
 		$this->icon   = 'evf-icon evf-icon-range-slider';
-		$this->order  = 170;
+		$this->order  = 140;
 		$this->group  = 'advanced';
 		$this->is_pro = true;
 
