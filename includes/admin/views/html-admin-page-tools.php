df86e8d66897e663af5ed3895abbf16994b87748
srijana <srijakunwar@gmail.com>
2021-02-02 14:31:50 +0545

    added navitation design for tools page

--- includes/admin/views/html-admin-page-tools.php
+++ includes/admin/views/html-admin-page-tools.php
@@ -23,7 +23,8 @@ $current_tab = ! empty( $_REQUEST['tab'] ) ? sanitize_title( wp_unslash( $_REQUE
 	<nav class="nav-tab-wrapper evf-nav-tab-wrapper">
 		<?php
 		foreach ( $tabs as $slug => $label ) {
-			echo '<a href="' . esc_html( admin_url( 'admin.php?page=evf-tools&tab=' . esc_attr( $slug ) ) ) . '" class="nav-tab ' . ( $current_tab === $slug ? 'nav-tab-active' : '' ) . '">' . esc_html( $label ) . '</a>';
+			// echo '<a href="' . esc_html( admin_url( 'admin.php?page=evf-tools&tab=' . esc_attr( $slug ) ) ) . '" class="nav-tab ' . ( $current_tab === $slug ? 'nav-tab-active' : '' ) . '">' . esc_html( $label ) . '</a>';
+			echo '<a href="' . esc_html( admin_url( 'admin.php?page=evf-settings&tab=' . esc_attr( $slug ) ) ) . '" class="nav-tab ' . ( $current_tab === $slug ? 'nav-tab-active' : '' ) . '"><span class="evf-nav-icon ' . esc_attr( $slug ) . '"></span>' . esc_html( $label ) . '</a>';
 		}
 		?>
 	</nav>
