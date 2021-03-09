969c61eff1c99c3f42e81fe9d84de5290f1d3499
Shiva Poudel <dev.shivapoudel@gmail.com>
2021-03-09 16:03:34 +0545

    Tweak - Duplicate total field for payment quantity field displayed

--- includes/fields/class-evf-field-payment-quantity.php
+++ includes/fields/class-evf-field-payment-quantity.php
@@ -16,9 +16,9 @@ class EVF_Field_Payment_Quantity extends EVF_Form_Fields {
 	 * Constructor.
 	 */
 	public function __construct() {
-		$this->name   = esc_html__( 'Total', 'everest-forms' );
-		$this->type   = 'payment-total';
-		$this->icon   = 'evf-icon evf-icon-total';
+		$this->name   = esc_html__( 'Quantity', 'everest-forms' );
+		$this->type   = 'payment-quantity';
+		$this->icon   = 'evf-icon evf-icon-single-item';
 		$this->order  = 40;
 		$this->group  = 'payment';
 		$this->is_pro = true;
