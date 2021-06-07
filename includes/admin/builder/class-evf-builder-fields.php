8338f1a58bbf1413b4c30d92b3ff7c0703c0e358
Milan Malla <milanmalla2015@gmail.com>
2021-06-07 21:05:59 +0545

    Enhancement - row clone

--- includes/admin/builder/class-evf-builder-fields.php
+++ includes/admin/builder/class-evf-builder-fields.php
@@ -250,6 +250,7 @@ class EVF_Builder_Fields extends EVF_Builder_Page {
 
 			echo '<div class="evf-admin-row" data-row-id="' . absint( $row ) . '">';
 			echo '<div class="evf-toggle-row">';
+			echo '<div class="evf-duplicate-row"><span class="dashicons dashicons-media-default" title="Duplicate"></span></div>';
 			echo '<div class="evf-delete-row"><span class="dashicons dashicons-trash" title="Delete"></span></div>';
 			echo '<div class="evf-show-grid"><span class="dashicons dashicons-edit" title="Edit"></span></div>';
 			echo '<div class="evf-toggle-row-content">';
