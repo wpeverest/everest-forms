73c1d51430898fa3210c17a816b4934ec765488c
Shiva Poudel <dev.shivapoudel@gmail.com>
2021-06-28 15:01:14 +0545

    Was deprecated in WP core, Ref: https://github.com/WordPress/wordpress-develop/commit/4ab922d22479074670fb015fd8ceaca70fb5adf7

--- phpunit/bootstrap.php
+++ phpunit/bootstrap.php
@@ -8,11 +8,6 @@
 // Require composer dependencies.
 require_once dirname( __DIR__ ) . '/vendor/autoload.php';
 
-// If we're running in WP's build directory, ensure that WP knows that, too.
-if ( 'build' === getenv( 'LOCAL_DIR' ) ) {
-	define( 'WP_RUN_CORE_TESTS', true );
-}
-
 // Determine the tests directory (from a WP dev checkout).
 // Try the WP_TESTS_DIR environment variable first.
 $_tests_dir = getenv( 'WP_TESTS_DIR' );
