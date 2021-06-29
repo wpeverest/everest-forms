31cdcf1ad652268b26c703c870d7b4640d8d3f10
Shiva Poudel <dev.shivapoudel@gmail.com>
2021-06-29 19:08:05 +0545

    Env

---
+++ tests/wp-tests-config.php
@@ -0,0 +1,34 @@
+<?php
+
+/* Path to the WordPress codebase you'd like to test. Add a forward slash in the end. */
+define( 'ABSPATH', getenv( 'WP_TESTS_DIR' ) ?: dirname( __FILE__, 6 ) . '/public/' );
+
+// Path to the theme to test with.
+define( 'WP_DEFAULT_THEME', 'default' );
+
+// Test with WordPress debug mode (default).
+define( 'WP_DEBUG', true );
+
+// ** MySQL settings ** //
+
+/*
+ * This configuration file will be used by the copy of WordPress being tested.
+ * wordpress/wp-config.php will be ignored.
+ *
+ * WARNING WARNING WARNING!
+ * These tests will DROP ALL TABLES in the database with the prefix named below.
+ * DO NOT use a production database or one that is shared with something else.
+ */
+define( 'DB_NAME',     getenv( 'WP_TESTS_DB_NAME' ) ?: 'wordpress_test' );
+define( 'DB_USER',     getenv( 'WP_TESTS_DB_USER' ) ?: 'root' );
+define( 'DB_PASSWORD', getenv( 'WP_TESTS_DB_PASS' ) ?: '' );
+define( 'DB_HOST',     getenv( 'WP_TESTS_DB_HOST' ) ?: 'localhost' );
+define( 'DB_CHARSET', 'utf8' );
+define( 'DB_COLLATE', '' );
+
+// Test suite configuration.
+define( 'WP_TESTS_DOMAIN', 'example.org' );
+define( 'WP_TESTS_EMAIL', 'admin@example.org' );
+define( 'WP_TESTS_TITLE', 'Test Blog' );
+define( 'WP_PHP_BINARY', 'php' );
+define( 'WPLANG', '' );
