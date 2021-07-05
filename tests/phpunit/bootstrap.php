f8060476b4e8051974f1c85c4a598ea00c605d76
Lekhnath Pandey <lekhnath005@gmail.com>
2021-07-05 08:48:25 +0545

    Dev - Test cases for core functions

--- tests/phpunit/bootstrap.php
+++ tests/phpunit/bootstrap.php
@@ -8,6 +8,13 @@
 // Require composer dependencies.
 require_once dirname( __FILE__ ) . '/../../vendor/autoload.php';
 
+WP_Mock::setUsePatchwork( true );
+WP_Mock::bootstrap( array(
+	'mock_constants' => false,
+	'mock_functions' => false,
+	'mock_hooks'     => false,
+));
+
 // Load tests environment variables.
 Dotenv\Dotenv::createUnsafeImmutable( dirname( __DIR__ ) )->safeLoad();
 
