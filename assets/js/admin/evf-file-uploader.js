8f1c6d1b1baee1d9f3f50e5449850a59fe48adb7
Milan Malla <milanmalla2015@gmail.com>
2021-07-27 20:11:11 +0545

    Remove button

--- assets/js/admin/evf-file-uploader.js
+++ assets/js/admin/evf-file-uploader.js
@@ -18,10 +18,10 @@ jQuery(document).ready(function($){
             // Let's assign the url value to the input field
             evf_uploader.attr('src', image_url);
             if( evf_uploader.hasClass( 'evf-button' ) ) {
-                evf_uploader.addClass( 'everest-forms-hidden' );
                 evf_uploader.prev().removeClass( 'everest-forms-hidden' );
                 evf_uploader.prev().attr('src', image_url);
                 evf_uploader.next().val(image_url);
+                evf_uploader.remove();
             } else {
                 evf_uploader.attr('src', image_url);
                 evf_uploader.next().next().val(image_url);
--- includes/admin/class-evf-admin-settings.php
+++ includes/admin/class-evf-admin-settings.php
@@ -325,7 +325,7 @@ if ( ! class_exists( 'EVF_Admin_Settings', false ) ) :
 							</th>
 							<td class="forminp forminp-<?php echo esc_attr( sanitize_title( $value['type'] ) ); ?>">&lrm;
 							<img src="<?php echo esc_attr( $option_value ); ?>" alt="<?php echo esc_attr__( 'Header Logo', 'everest-forms' ); ?>" class="evf-image-uploader <?php echo empty( $option_value ) ? 'everest-forms-hidden' : ''; ?>" height="100" width="auto">
-							<button class="evf-image-uploader evf-button button button-secondary <?php echo empty( $option_value ) ? '' : 'everest-forms-hidden'; ?>" style="padding:5px 20px;"><?php echo esc_html__( 'Upload Logo', 'everest-forms' ); ?></button>
+							<button class="evf-image-uploader evf-button button-secondary <?php echo empty( $option_value ) ? '' : 'everest-forms-hidden'; ?>"><?php echo esc_html__( 'Upload Logo', 'everest-forms' ); ?></button>
 							<input
 								name="<?php echo esc_attr( $value['id'] ); ?>"
 								id="<?php echo esc_attr( $value['id'] ); ?>"
