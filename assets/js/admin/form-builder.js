55d0c7c2b46f763dd08079a09c929dc7bae7ca50
Shiva Poudel <dev.shivapoudel@gmail.com>
2021-08-05 17:15:44 +0545

    JS

--- assets/js/admin/form-builder.js
+++ assets/js/admin/form-builder.js
@@ -2060,7 +2060,7 @@
 				scrollSensitivity: 40,
 				forcePlaceholderSize: true,
 				helper: function(event) {
-					if ( 'evf-icon evf-icon-repeater-fields' == event.target.className ){
+					if ( 'evf-icon evf-icon-repeater' == event.target.className ){
 						EVFPanelBuilder.bindAddNewRepeaterRow();
 					}
 					return $( this ).clone().insertAfter( $( this ).closest( '.everest-forms-tab-content' ).siblings( '.everest-forms-fields-tab' ) );
