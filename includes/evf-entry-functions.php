6aa860785a61668e4f4310f38449f0612e33ef99
Milan Malla <milanmalla2015@gmail.com>
2021-09-07 22:11:56 +0545

    Draft Entry removed

--- includes/evf-entry-functions.php
+++ includes/evf-entry-functions.php
@@ -204,6 +204,9 @@ function evf_search_entries( $args ) {
 		}
 	}
 
+	// Removing Draft Entry (Save and Contd Add-on).
+	$query[] = $wpdb->prepare( 'AND `status` <> %s', 'draft' );
+
 	$valid_fields = array( 'date', 'form_id', 'title', 'status' );
 	$orderby      = in_array( $args['orderby'], $valid_fields, true ) ? $args['orderby'] : 'entry_id';
 	$order        = 'DESC' === strtoupper( $args['order'] ) ? 'DESC' : 'ASC';
@@ -299,7 +302,7 @@ function evf_get_entries_by_form_id( $form_id, $start_date = '', $end_date = '' 
 	global $wpdb;
 
 	$query   = array();
-	$query[] = $wpdb->prepare( "SELECT * FROM {$wpdb->prefix}evf_entries WHERE form_id=%s AND status != 'draft'", $form_id );
+	$query[] = $wpdb->prepare( "SELECT * FROM {$wpdb->prefix}evf_entries WHERE form_id=%s", $form_id );
 
 	if ( ! empty( $start_date ) ) {
 		$query[] = $wpdb->prepare( 'AND date_created  >= %s', $start_date );
@@ -308,6 +311,8 @@ function evf_get_entries_by_form_id( $form_id, $start_date = '', $end_date = '' 
 	if ( ! empty( $end_date ) ) {
 		$query[] = $wpdb->prepare( 'AND date_created  <= %s', $end_date );
 	}
+
+	$query[] = $wpdb->prepare( 'AND status != %s', 'draft' );
 
 	$results = wp_cache_get( $form_id, 'evf-search-entries' );
 
