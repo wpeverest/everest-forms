3ec9732e1e524ebec66b0d04cb3d17d092adc47e
Rabin Shrestha <sun.ravi90@gmail.com>
2021-10-04 12:47:31 +0545

    Update survey messages

--- includes/admin/views/html-notice-survey.php
+++ includes/admin/views/html-notice-survey.php
@@ -13,13 +13,14 @@ defined( 'ABSPATH' ) || exit;
 		<svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 24 24"><path d="M18.15,4l1.23,2H15.49L14.26,4ZM20,20H2.21L12,4.09,18.1,14H10.77L12,12h2.52L12,7.91,5.79,18H20.56l1.23,2ZM17.94,10,16.71,8H20.6l1.23,2Z"/></svg>
 	</div>
 	<div class="everest-forms-message--content">
-		<h3 class="everest-forms-message__title"><?php esc_html_e( 'Everest Form Plugin Survey', 'everest-forms' ); ?></h3>
+		<h3 class="everest-forms-message__title"><?php esc_html_e( 'Everest Forms Survey!', 'everest-forms' ); ?></h3>
 		<p class="everest-forms-message__description">
 		<p>
 		<?php
 		_e(
-			'<strong>Hey there!</strong> <br>
-								We would be grateful if you could spare a moment and help us fill this survey. This survey will take approximately 4 minutes to complete.',
+			'Hey there!<br>
+			<p>We would be grateful if you could spare a moment and help us fill this survey<p>
+			<p>This survey will take approximately 4 minutes to complete.<p>',
 			'everest-forms'
 		);
 		?>
@@ -27,18 +28,16 @@ defined( 'ABSPATH' ) || exit;
 		<p class="extra-pad">
 		<?php
 		_e(
-			'<strong>What benefit would you have?</strong> <br>
-								We will take your feedback from the survey and use that information to make the plugin better. As a result, you will have a better plugin as you wanted. <span class="dashicons dashicons-smiley smile-icon"></span><br>',
+			'<strong>What benefit would you have?</strong><br>
+			We will take your feedback from the survey which will eventually help to improve the Everest Forms plugin. Thank you in advance for participating. <span class="dashicons dashicons-smiley smile-icon"></span><br>',
 			'everest-forms'
 		);
 		?>
 		</p>
 		</p>
 		<p class="everest-forms-message__action submit">
-			<a href="https://survey.wpeverest.com/everest-forms/" class="button button-primary evf-dismiss-review-notice evf-survey-received" target="_blank" rel="noopener noreferrer"><span class="dashicons dashicons-external"></span><?php esc_html_e( 'Sure, I\'d love to!', 'everest-forms' ); ?></a>
+			<a href="https://survey.wpeverest.com/everest-forms/" class="button button-primary evf-dismiss-review-notice evf-survey-received" target="_blank" rel="noopener noreferrer"><span class="dashicons dashicons-external"></span><?php esc_html_e( 'Start Survey', 'everest-forms' ); ?></a>
 			<a href="#" class="button button-secondary evf-dismiss-survey-notice" target="_blank" rel="noopener noreferrer"><span  class="dashicons dashicons-smiley"></span><?php esc_html_e( 'I already did', 'everest-forms' ); ?></a>
-			<a href="#" class="button button-secondary evf-dismiss-survey-notice" target="_blank" rel="noopener noreferrer"><span class="dashicons dashicons-dismiss"></span><?php esc_html_e( 'Never show again  ', 'everest-forms' ); ?></a>
-			<a href="https://wpeverest.com/support-forum/" class="button button-secondary evf-have-query" target="_blank" rel="noopener noreferrer"><span class="dashicons dashicons-testimonial"></span><?php esc_html_e( 'I have a query', 'everest-forms' ); ?></a>
 		</p>
 	</div>
 </div>
