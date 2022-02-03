ba10eb11a42d3e672a6b20ba4e0cccc6a86ec779
Ganesh Bhatta <bhattaganesh205@gmail.com>
2022-02-03 13:07:41 +0545

    Escaping on value and palceholder.

--- includes/abstracts/class-evf-form-fields.php
+++ includes/abstracts/class-evf-form-fields.php
@@ -1437,8 +1437,8 @@ abstract class EVF_Form_Fields {
 							$field,
 							array(
 								'slug'        => 'whitelist_domain',
-								'value'       => $value,
-								'placeholder' => __( 'for eg. gmail.com', 'everest-forms' ),
+								'value'       => esc_attr( $value ),
+								'placeholder' => esc_attr__( 'for eg. gmail.com', 'everest-forms' ),
 							),
 							false
 						),
