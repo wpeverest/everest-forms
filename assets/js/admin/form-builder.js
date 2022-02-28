5e350452f8d5df5e4456949195122c169ab34760
Milan Malla <34101157+milan1750@users.noreply.github.com>
2022-02-28 13:48:09 +0545

    Enhance - Enable field Drag to another part (#847)
    * Enhance - Enable field Drag to another part
    
    * field dragable css issue fixed
    
    * update
    
    * field drag issue fixed
    
    * field drag css issue fixed
    
    * tweak
    
    Co-authored-by: Prajwol321 <prajwol.themegrill@gmail.com>

--- assets/js/admin/form-builder.js
+++ assets/js/admin/form-builder.js
@@ -20,7 +20,7 @@
 		 	});
 
 
-			 $(document).ready( function( $ ) {
+			$( document ).ready( function( $ ) {
 				 if( '1' === $( '.everest-forms-min-max-date-format input' ).val() ) {
 					$('.everest-forms-min-date').addClass('flatpickr-field').flatpickr({
 						disableMobile : true,
@@ -45,10 +45,10 @@
 			});
 
 
-		 		if ( ! $( 'evf-panel-payments-button a' ).hasClass( 'active' ) ) {
-		 			$( '#everest-forms-panel-payments' ).find( '.everest-forms-panel-sidebar a' ).first().addClass( 'active' );
-					$( '.everest-forms-panel-content' ).find( '.evf-payment-setting-content' ).first().addClass( 'active' );
-				}
+			if ( ! $( 'evf-panel-payments-button a' ).hasClass( 'active' ) ) {
+				$( '#everest-forms-panel-payments' ).find( '.everest-forms-panel-sidebar a' ).first().addClass( 'active' );
+				$( '.everest-forms-panel-content' ).find( '.evf-payment-setting-content' ).first().addClass( 'active' );
+			}
 
 
 			// Copy shortcode from the builder.
@@ -801,6 +801,14 @@
 				e.preventDefault();
 				EVFPanelBuilder.fieldTabChoice( $(this).attr( 'id' ) );
 			});
+
+
+			// Dragged field and hover over tab buttons - multipart.
+			$(document).on( 'mouseenter', '.everest-forms-tabs li[class*="part_"]', function() {
+				if ( false === $( this ).hasClass( 'active' ) && ( $( document ).find( '.everest-forms-field' ).hasClass( 'ui-sortable-helper' ) || $( document ).find( '.evf-registered-buttons button.evf-registered-item' ).hasClass( 'field-dragged' ) ) ) {
+					$( this ).find( 'a' ).trigger( 'click' );
+				}
+			} );
 
 			// Display toggle for "Address" field hidden option.
 			$builder.on( 'change', '.everest-forms-field-option-address input.hide', function() {
@@ -2021,7 +2029,9 @@
 				scrollSensitivity: 40,
 				forcePlaceholderSize: true,
 				connectWith: '.evf-admin-grid',
+				appendTo: document.body,
 				containment: '.everest-forms-field-wrap',
+
 				out: function( event ) {
 					$( '.evf-admin-grid' ).removeClass( 'evf-hover' );
 					$( event.target ).removeClass( 'evf-item-hover' );
@@ -2055,8 +2065,14 @@
 				revert: 'invalid',
 				scrollSensitivity: 40,
 				forcePlaceholderSize: true,
+				start: function() {
+					$( this ).addClass( 'field-dragged' );
+				},
 				helper: function() {
 					return $( this ).clone().insertAfter( $( this ).closest( '.everest-forms-tab-content' ).siblings( '.everest-forms-fields-tab' ) );
+				},
+				stop: function() {
+					$( this ).removeClass( 'field-dragged' );
 				},
 				opacity: 0.75,
 				containment: '#everest-forms-builder',
