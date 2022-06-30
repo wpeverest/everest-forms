b300faecdc6cc3a6129ff9e8f5230d83be02f02f
Ganesh Bhatta <49543894+bhattaganesh@users.noreply.github.com>
2022-06-30 10:18:56 +0545

    Fix - min/max value glitch and default value validation. (#923)
    * min-max glitches and default value validation for number and range slider field.
    
    * update - error tool tip for two column Min/Max option UI.
    
    * Default value for step set to zero.
    
    * fix - two column Min/Max option UI css
    
    * Changelog added for #923

--- assets/js/admin/admin.js
+++ assets/js/admin/admin.js
@@ -139,7 +139,7 @@
 		  }
 		})
 
-		.on('click','.everest-forms-field-number', function(e) {
+		.on('click','.everest-forms-field-number, .everest-forms-field-range-slider', function(e) {
 			var $this = $(this);
 			var id = $this.data('field-id');
 			$(document).on('keydown click',"#everest-forms-field-option-"+ id +"-default_value",function(e){
@@ -147,6 +147,33 @@
 				|| ( e.keyCode > 47 && e.keyCode < 58 )
 				|| e.keyCode == 8 ) ) {
 				  return false;
+				}
+			})
+
+			$(document).on( 'keyup focus',"#everest-forms-field-option-"+ id +"-default_value",function( e ){
+				var fieldId  = $( this ).parent().data( 'fieldId' ) ? $( this ).parent().data( 'fieldId' ) : $( this ).closest( '.everest-forms-field-option-row' ).data( 'field-id' );
+				var maxField = $( "input#everest-forms-field-option-"+fieldId+"-max_value" );
+				var minField = $( "input#everest-forms-field-option-"+fieldId+"-min_value" );
+				var maxVal   = maxField.val();
+				var minVal   = minField.val();
+				var defVal   = e.target.value;
+				var $this 	 = $( this );
+
+				if ( 0 !== minVal.length  ) {
+
+					if( 0 !== maxVal.length && parseFloat( defVal ) > parseFloat( maxVal ) ) {
+						$( document.body ).triggerHandler( 'evf_remove_error_tip', [ $this, 'i18n_field_def_value_smaller' ] );
+						$( document.body ).triggerHandler( 'evf_add_error_tip', [ $this, 'i18n_field_def_value_greater', params ] );
+					} else  {
+						$( document.body ).triggerHandler( 'evf_remove_error_tip', [ $this, 'i18n_field_def_value_greater' ] );
+					} 
+
+					if( 0 !== defVal.length && parseFloat( defVal ) < parseFloat( minVal ) ) {
+						$( document.body ).triggerHandler( 'evf_remove_error_tip', [ $this, 'i18n_field_def_value_greater' ] );
+						$( document.body ).triggerHandler( 'evf_add_error_tip', [ $this, 'i18n_field_def_value_smaller', params ] );
+					} else {
+						$( document.body ).triggerHandler( 'evf_remove_error_tip', [ $this, 'i18n_field_def_value_smaller' ] );
+					}
 				}
 			})
 		})
