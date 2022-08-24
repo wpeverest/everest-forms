38d5cb493dc625e9479fb968f89582c72cb5c294
Ganesh Bhatta <bhattaganesh05@gmail.com>
2022-08-24 13:30:45 +0545

    fix-upload image button text issue

--- includes/admin/evf-admin-functions.php
+++ includes/admin/evf-admin-functions.php
@@ -480,7 +480,7 @@ function everest_forms_panel_field( $option, $panel, $field, $form_data, $label,
 			$output .= sprintf( '<a href="#" class="everest-forms-custom-image-delete"><img src="%1$s" alt="' . __( ' %2$s', 'everest-forms' ) . '" class="evf-custom-image-uploader %3$s" height="100" width="auto">', esc_attr( $value ), esc_attr( $alt ), ( empty( $value ) ? 'everest-forms-hidden' : '' ) ); // phpcs:ignore
 			$output .= sprintf( '</a></div>' );
 			/* translators: %2$s : Upload Image button Text. */
-			$output .= sprintf( '<div class="everest-forms-custom-image-button"><button type="button" class="evf-custom-image-uploader-button evf-custom-image-button %1$s">' . __( '%s', 'everest-forms' ) . '</button>', ( empty( $value ) ? 'button-secondary' : 'everest-forms-hidden' ), esc_html( $button_text ) ); // phpcs:ignore
+			$output .= sprintf( '<div class="everest-forms-custom-image-button"><button type="button" class="evf-custom-image-uploader-button evf-custom-image-button %1$s">' . __( '%2$s', 'everest-forms' ) . '</button>', ( empty( $value ) ? 'button-secondary' : 'everest-forms-hidden' ), esc_html( $button_text ) ); // phpcs:ignore
 			$output .= sprintf(
 				'<input type="hidden" id="everest-forms-panel-field-%s-%s" name="%s" value="%s" placeholder="%s" class="widefat %s" %s></div>',
 				sanitize_html_class( $panel_id ),
