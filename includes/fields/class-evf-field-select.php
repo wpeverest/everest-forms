b3558cdad6a0a5eca6d02d6cf273f5f5ec8e9830
Ganesh Bhatta <bhattaganesh05@gmail.com>
2022-09-13 10:00:55 +0545

    update-paramter for dropdown options filter

--- includes/fields/class-evf-field-select.php
+++ includes/fields/class-evf-field-select.php
@@ -88,7 +88,8 @@ class EVF_Field_Select extends EVF_Form_Fields {
 		// Define data.
 		$form_id  = absint( $form_data['id'] );
 		$field_id = $field['id'];
-		$choices  = apply_filters( 'everest_forms_dropdown_options', $field['choices'] );
+		$field    = apply_filters( 'everest_forms_dropdown_options', $field );
+		$choices  = $field['choices'];
 
 		// Remove primary input.
 		unset( $properties['inputs']['primary'] );
