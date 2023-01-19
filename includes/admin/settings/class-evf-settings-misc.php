9f9a36a9d5206efceb40335b2a77422e3b6cb737
Ganesh Bhatta <bhattaganesh05@gmail.com>
2023-01-19 14:01:47 +0545

    Misc setting tab

---
+++ includes/admin/settings/class-evf-settings-misc.php
@@ -0,0 +1,72 @@
+<?php
+/**
+ * EverestForms Misc Settings
+ *
+ * @package EverestForms\Admin
+ * @version 1.9.8
+ */
+
+defined( 'ABSPATH' ) || exit;
+
+if ( class_exists( 'EVF_Settings_Misc', false ) ) {
+	return new EVF_Settings_Misc();
+}
+
+/**
+ * EVF_Settings_Misc.
+ */
+class EVF_Settings_Misc extends EVF_Settings_Page {
+
+	/**
+	 * Constructor.
+	 */
+	public function __construct() {
+		$this->id    = 'misc';
+		$this->label = esc_html__( 'Misc', 'everest-forms' );
+
+		parent::__construct();
+	}
+
+	/**
+	 * Get settings array.
+	 *
+	 * @return array
+	 */
+	public function get_settings() {
+		$settings = apply_filters(
+			'everest_forms_misc_settings',
+			array(
+				array(
+					'title' => esc_html__( 'Advanced Options', 'everest-forms' ),
+					'type'  => 'title',
+					'desc'  => '',
+					'id'    => 'misc_options',
+				),
+				array(
+					'title'   => esc_html__( 'Uninstall Everest Forms', 'everest-forms' ),
+					'desc'    => __( '<strong>Heads Up!</strong> Check this if you would like to remove ALL Everest Forms data upon plugin deletion.', 'everest-forms' ),
+					'id'      => 'everest_forms_misc_setting_uninstall_option',
+					'default' => 'no',
+					'type'    => 'checkbox',
+				),
+				array(
+					'type' => 'sectionend',
+					'id'   => 'misc_options',
+				),
+			)
+		);
+
+		return apply_filters( 'everest_forms_get_settings_' . $this->id, $settings );
+	}
+
+	/**
+	 * Save settings.
+	 */
+	public function save() {
+		$settings = $this->get_settings();
+
+		EVF_Admin_Settings::save_fields( $settings );
+	}
+}
+
+return new EVF_Settings_Misc();
