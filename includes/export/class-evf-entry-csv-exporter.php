134efe0a07c61fc69e694772143a27f7443863ec
deepench <elendoff11@gmail.com>
2023-05-05 09:37:19 +0545

    Fix - depricated error

--- includes/export/class-evf-entry-csv-exporter.php
+++ includes/export/class-evf-entry-csv-exporter.php
@@ -379,7 +379,11 @@ class EVF_Entry_CSV_Exporter extends EVF_CSV_Exporter {
 			}
 			$column_type       = $this->get_entry_type( $column_id, $entry );
 			$row[ $column_id ] = apply_filters( 'everest_forms_format_csv_field_data', preg_match( '/textarea/', $column_type ) ? sanitize_textarea_field( $value ) : sanitize_text_field( $value ), $raw_value, $column_id, $column_name, $columns, $entry );
-			$row['status']     = isset( $entry->meta['status'] ) && ! empty( $entry->meta['status'] ) ? $entry->meta['status'] : isset( $row['status'] ) ? $row['status'] : '';
+			$row['status']     = (
+				isset( $entry->meta['status'] ) && ! empty( $entry->meta['status'] )
+				? $entry->meta['status']
+				: ( isset( $row['status'] ) ? $row['status'] : '' )
+			);
 		}
 
 		return apply_filters( 'everest_forms_entry_export_row_data', $row, $entry, $this->request_data );
