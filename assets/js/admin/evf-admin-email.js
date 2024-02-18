d5acdc29985b0ffc62e67562a4d4a2b89f446305
Ritee Lama <89212873+riteelama@users.noreply.github.com>
2024-02-18 21:05:19 +0545

    Feature - Email preview (#1193)
    * Added - Preview Link on eye icon
    
    * Added - Redirected to preview page
    
    * Added - Preview the message on nect page without parsing
    
    * Added -  Function to render the message on the preview page
    
    * Added - Form id to the preview link
    
    * Added - Template to the preview email message
    
    * Added - Template for the email on preview
    
    * Added - Form fields data validation
    
    * Added - Preview option to the new email connection
    
    * Added - Dummy data for all_fields smart tags for email template preview
    
    * Fixed - PHPCS fixed
    
    * Fixed - PHPCS on template-loader
    
    ---------
    
    Co-authored-by: Deependra Chaudhary <elendoff11@gmail.com>

--- assets/js/admin/evf-admin-email.js
+++ assets/js/admin/evf-admin-email.js
@@ -122,8 +122,10 @@
 		addNewEmailConnection: function( el, data ){
 			var $this= el;
 			var response = data.response;
+			var preview_url = response.data.preview_url;
 			var name = data.name;
 			var $connections = $this.closest('.everest-forms-panel-sidebar-content');
+			var $connections_list = $connections.find('.everest-forms-panel-sidebar');
 			var form_title = $('#everest-forms-panel-field-settings-form_title:first').val() + '-' + Date.now();
 			var cloned_email = $('.evf-content-email-settings').first().clone();
 			$('.evf-content-email-settings-inner').removeClass('active-connection');
@@ -236,6 +238,9 @@
 			cloned_email.find('.evf-field-conditional-input').attr('name', 'settings[email]['+response.data.connection_id+'][conditionals][1][1][value]');
 			$cloned_email = cloned_email.append('<input type="hidden" name="settings[email]['+response.data.connection_id+'][connection_name]" value="'+name+'">');
 
+			// Grabs the address of the default connection to preview the message
+			var cloned_email_preview_link = $connections_list.find('.email-default-preview').attr('href');
+
 			$('.evf-email-settings-wrapper').append(cloned_email);
 
 			$connections.find('.evf-content-email-settings-inner').last().addClass('active-connection');
@@ -260,6 +265,15 @@
 							'</svg></span>' +
 						'</a>' +
 						'<span class="evf-vertical-divider"></span>' +
+							'<a href="'+preview_url+'" class="evf-email-preview" target="_blank">' +
+								'<span class="email-preview">' +
+								'<svg  xmlns="http://www.w3.org/2000/svg" xmlns="http://www.w3.org/1999/svg"' +
+								'viewBox="0 0 442.04 442.04" xml:space="preserve"><g id="SVGRepo_bgCarrier" stroke-width="0"></g>' +
+								'<g id="SVGRepo_tracerCarrier" stroke-linecap="round" stroke-linejoin="round"></g>' +
+								'<g id="SVGRepo_iconCarrier"> <g> <g>' +
+									'<path d="M221.02,341.304c-49.708,0-103.206-19.44-154.71-56.22C27.808,257.59,4.044,230.351,3.051,229.203 c-4.068-4.697-4.068-11.669,0-16.367c0.993-1.146,24.756-28.387,63.259-55.881c51.505-36.777,105.003-56.219,154.71-56.219 c49.708,0,103.207,19.441,154.71,56.219c38.502,27.494,62.266,54.734,63.259,55.881c4.068,4.697,4.068,11.669,0,16.367 c-0.993,1.146-24.756,28.387-63.259,55.881C324.227,321.863,270.729,341.304,221.02,341.304z M29.638,221.021 c9.61,9.799,27.747,27.03,51.694,44.071c32.83,23.361,83.714,51.212,139.688,51.212s106.859-27.851,139.688-51.212 c23.944-17.038,42.082-34.271,51.694-44.071c-9.609-9.799-27.747-27.03-51.694-44.071 c-32.829-23.362-83.714-51.212-139.688-51.212s-106.858,27.85-139.688,51.212C57.388,193.988,39.25,211.219,29.638,221.021z"></path> </g> <g> <path d="M221.02,298.521c-42.734,0-77.5-34.767-77.5-77.5c0-42.733,34.766-77.5,77.5-77.5c18.794,0,36.924,6.814,51.048,19.188 c5.193,4.549,5.715,12.446,1.166,17.639c-4.549,5.193-12.447,5.714-17.639,1.166c-9.564-8.379-21.844-12.993-34.576-12.993 c-28.949,0-52.5,23.552-52.5,52.5s23.551,52.5,52.5,52.5c28.95,0,52.5-23.552,52.5-52.5c0-6.903,5.597-12.5,12.5-12.5 s12.5,5.597,12.5,12.5C298.521,263.754,263.754,298.521,221.02,298.521z"></path> </g> <g> <path d="M221.02,246.021c-13.785,0-25-11.215-25-25s11.215-25,25-25c13.786,0,25,11.215,25,25S234.806,246.021,221.02,246.021z"></path>' +
+								'</g> </g> </g></svg>' +
+							'</a>' +
 						'<a href="#" class="everest-forms-email-duplicate">'+
 							'<span class="email-duplicate">' +
 							'<svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 24 25">' +
