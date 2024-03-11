8ec7745cd101db670bf3c498beb5502fcfb30b9e
NirajChaudhary143 <nirajchy2000@gmail.com>
2024-03-11 14:10:26 +0545

    Signature And Likert Preview Issue Solved

--- includes/admin/class-evf-admin-preview-confirmation.php
+++ includes/admin/class-evf-admin-preview-confirmation.php
@@ -21,7 +21,7 @@ class EVF_Admin_Preview_Confirmation {
 	 * @since 2.0.8
 	 */
 	public static function init() {
-		add_action( 'everest_forms_preview_confirmation', array( __CLASS__, 'preview_confirmation' ), 20, 3 );
+		add_action( 'everest_forms_preview_confirmation', array( __CLASS__, 'preview_confirmation' ), 20, 4 );
 		add_filter( 'everest_forms_notice_types', array( __CLASS__, 'add_notice' ) );
 	}
 
@@ -67,27 +67,40 @@ class EVF_Admin_Preview_Confirmation {
 	 *
 	 * @param  [array]  $form_data   Form Data.
 	 * @param  [array]  $entry Entry.
+	 * @param [array]  $form_fields Form Fields.
 	 * @param [string] $preview_style Preview Style.
 	 */
-	public static function preview_confirmation( $form_data, $entry, $preview_style ) {
+	public static function preview_confirmation( $form_data, $entry, $form_fields, $preview_style ) {
 		$output  = '';
 		$output .= '<div class="everest_forms_preview_confirmation_' . $preview_style . '">';
+
 		foreach ( $form_data['form_fields'] as $id => $data ) {
+
+			if ( ! isset( $entry['form_fields'] ) ) {
+				continue;
+			}
+			$form_field_type = $form_data['form_fields'][ $id ]['type'];
+
+			$signature = apply_filters( 'everest_forms_field_exporter_' . $form_field_type, $form_fields[ $id ], 'pdf-export' );
+
 			$output .= '<div class="everest_forms_preview_confirmation_row_title_' . $preview_style . '">' . $data['label'] . ': ';
 			if ( 'basic' === $preview_style ) {
 				$output .= '</div>';
 
 				if ( is_array( $entry['form_fields'][ $id ] ) ) {
 
-					if ( 'likert' === $form_data['form_fields'][ $id ]['type'] ) {
+					if ( 'likert' === $form_field_type ) {
 						$output .= self::likert_output( $form_data, $entry, $id );
 					}
 
-					if ( 'checkbox' === $form_data['form_fields'][ $id ]['type'] ) {
+					if ( 'checkbox' === $form_field_type ) {
 						$output .= '<div class="everest_forms_preview_confirmation_row_data_' . $preview_style . '">';
 						foreach ( $entry['form_fields'][ $id ] as $value ) {
 							$output .= $value . '</div>';
 						}
+					}
+					if ( 'signature' === $form_field_type ) {
+						$output .= '<div>' . $signature['value'] . '</div>';
 					}
 				} else {
 					$output .= '<div class="everest_forms_preview_confirmation_row_data_' . $preview_style . '">' . $entry['form_fields'][ $id ] . '</div>';
@@ -96,10 +109,10 @@ class EVF_Admin_Preview_Confirmation {
 				if ( is_array( $entry['form_fields'][ $id ] ) ) {
 
 					$entry_count = count( $entry['form_fields'][ $id ] );
-					if ( 'likert' === $form_data['form_fields'][ $id ]['type'] ) {
+					if ( 'likert' === $form_field_type ) {
 						$output .= self::likert_output( $form_data, $entry, $id );
 					}
-					if ( 'checkbox' === $form_data['form_fields'][ $id ]['type'] ) {
+					if ( 'checkbox' === $form_field_type ) {
 						foreach ( $entry['form_fields'][ $id ] as $value ) {
 							--$entry_count;
 							$output .= $value;
@@ -107,7 +120,10 @@ class EVF_Admin_Preview_Confirmation {
 								$output .= ', ';
 							}
 						}
-						$output .= '</div>';
+						$output .= '</d>';
+					}
+					if ( 'signature' === $form_field_type ) {
+						$output .= $signature['value'] . '</div>';
 					}
 				} else {
 					$output .= $entry['form_fields'][ $id ] . '</div>';
