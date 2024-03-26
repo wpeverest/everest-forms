8ca2600b7a0abce3c6bb18285ed214e2639a7cb7
MILAN88888 <chaudharymilan996@gmail.com>
2024-03-26 16:37:16 +0545

    Add - Dashboard screen

--- src/dashboard/App.js
+++ src/dashboard/App.js
@@ -1,21 +1,21 @@
-import React from 'react'
-import { HashRouter } from 'react-router-dom'
-import { Container, ChakraProvider } from '@chakra-ui/react'
-import Theme from './Theme/Theme'
-import Router from './Router/Router'
-import { Header } from './components'
+import React from "react";
+import { HashRouter } from "react-router-dom";
+import { Container, ChakraProvider } from "@chakra-ui/react";
+import Theme from "./Theme/Theme";
+import Router from "./Router/Router";
+import { Header } from "./components";
 
 const App = () => {
-	return (
-		<HashRouter>
-			<ChakraProvider theme={Theme}>
-				<Header />
-				<Container maxW='container.xl'>
-					<Router />
-				</Container>
-			</ChakraProvider>
-		</HashRouter>
-	)
-}
+  return (
+    <HashRouter>
+      <ChakraProvider theme={Theme}>
+        <Header />
+        <Container maxW="container.xl">
+          <Router />
+        </Container>
+      </ChakraProvider>
+    </HashRouter>
+  );
+};
 
-export default App
+export default App;
--- src/dashboard/index.js
+++ src/dashboard/index.js
@@ -1,14 +1,22 @@
-import React from 'react'
-import ReactDOM from 'react-dom/client'
-import App from './App'
+import React from "react";
+import ReactDOM from "react-dom/client";
+import App from "./App";
+import dashboardReducer, { initialState } from "./reducers/DashboardReducer";
+import { DashboardProvider } from "./context/DashboardContext";
+(function() {
+  const container = document.getElementById("everest-forms-dashboard");
 
-(function () {
-	const container = document.getElementById('everest-forms-dashboard')
+  if (!container) return;
 
-	if (!container) return
-
-	const root = ReactDOM.createRoot(container)
-	if (root) {
-		root.render(<App />)
-	}
-})()
+  const root = ReactDOM.createRoot(container);
+  if (root) {
+    root.render(
+      <DashboardProvider
+        initialState={initialState}
+        dashboardReducer={dashboardReducer}
+      >
+        <App />
+      </DashboardProvider>
+    );
+  }
+})();
