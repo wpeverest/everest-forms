6a5a26c8bce8632f54dbbff86b6c2eea136f32cf
Deependra Chaudhary <elendoff11@gmail.com>
2024-04-30 10:13:39 +0545

    Fix - phpcs issue

--- includes/class-everest-forms.php
+++ includes/class-everest-forms.php
@@ -452,7 +452,7 @@ final class EverestForms {
 	 * @return string
 	 */
 	public function ajax_url() {
-		 return admin_url( 'admin-ajax.php', 'relative' );
+		return admin_url( 'admin-ajax.php', 'relative' );
 	}
 
 	/**
