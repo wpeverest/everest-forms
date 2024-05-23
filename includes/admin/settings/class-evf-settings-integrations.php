db583bd6852a34bc4e576be9c319095bce53a83c
Ayush Shrestha <ayushstha143@gmail.com>
2024-05-23 10:35:37 +0545

    global settings and form builder pro section opacity fixed

--- includes/admin/settings/class-evf-settings-integrations.php
+++ includes/admin/settings/class-evf-settings-integrations.php
@@ -56,8 +56,12 @@ class EVF_Settings_Integrations extends EVF_Settings_Page {
 	 */
 	protected function output_integrations( $integrations ) {
 		?>
-		<span class="evf-forms-options-header-header--top-icon"><?php echo evf_file_get_contents( '/assets/images/settings-icons/integration.svg' ); //phpcs:ignore ?></span>
-		<h2><?php esc_html_e( 'Integrations', 'everest-forms' ); ?></h2>
+		<div class="everest-forms-options-header">
+			<div class="everest-forms-options-header--top">
+				<span class="evf-forms-options-header-header--top-icon"><?php echo evf_file_get_contents( '/assets/images/settings-icons/integration.svg' ); //phpcs:ignore ?></span>
+				<h3><?php esc_html_e( 'Integrations', 'everest-forms' ); ?></h3>
+			</div>
+		</div>
 		<div class="everest-forms-integrations-connection">
 			<?php foreach ( $integrations as $integration ) : ?>
 				<div class="everest-forms-integrations" data-action="<?php echo esc_attr( isset( $integration->upgrade ) ? $integration->upgrade : '' ); ?>" data-links="<?php echo esc_attr( isset( $integration->vedio_id ) ? $integration->vedio_id : '' ); ?>">
