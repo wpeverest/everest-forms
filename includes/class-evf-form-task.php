e9e7a9db953a963d6c9aab266fdfba0fa307211f
Sujit Shrestha <sujitshrestha0004@gmal.com>
2024-06-13 13:58:48 +0545

    Refactor

--- includes/class-evf-form-task.php
+++ includes/class-evf-form-task.php
@@ -600,8 +600,8 @@ class EVF_Form_Task {
 			$response_data['response']                  = 'success';
 			$response_data['form_id']                   = $form_id;
 			$response_data['entry_id']                  = $entry_id;
-			$response_data['message_display_location']  = isset( $this->form_data['settings']['successful_form_submission_message_display_location'] ) && 'null' !== $this->form_data['settings']['successful_form_submission_message_display_location'] ? $this->form_data['settings']['successful_form_submission_message_display_location'] : 'hide';
-			$response_data['submission_message_scroll'] = isset( $this->form_data['settings']['submission_message_scroll'] ) && 'null' !== $this->form_data['settings']['submission_message_scroll'] ? $this->form_data['settings']['submission_message_scroll'] : '1';
+			$response_data['message_display_location']  = isset( $settings['successful_form_submission_message_display_location'] ) && 'null' !== $settings['successful_form_submission_message_display_location'] ? $settings['successful_form_submission_message_display_location'] : 'hide';
+			$response_data['submission_message_scroll'] = isset( $settings['submission_message_scroll'] ) && 'null' !== $settings['submission_message_scroll'] ? $this->form_data['settings']['submission_message_scroll'] : '1';
 			if ( defined( 'EVF_PDF_SUBMISSION_VERSION' ) && ( 'yes' === get_option( 'everest_forms_pdf_download_after_submit', 'no' ) || ( isset( $pdf_submission['everest_forms_pdf_download_after_submit'] ) && 'yes' === $pdf_submission['everest_forms_pdf_download_after_submit'] ) ) ) {
 				$response_data['pdf_download'] = true;
 				$pdf_download_message          = get_option( 'everest_forms_pdf_custom_download_text', '' );
