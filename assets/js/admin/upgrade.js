de187dd06a90f496af358d39d5e9d604b682e2d6
riteelama <riteelama@gmail.com>
2024-06-18 16:25:29 +0545

    Add - Verifone credit card and validation

--- assets/js/admin/upgrade.js
+++ assets/js/admin/upgrade.js
@@ -9,6 +9,7 @@ jQuery( function( $ ) {
 			$( document.body ).on( 'click dragstart', '.evf-registered-item.evf-upgrade-addon', this.evf_upgrade_addon );
 			$( document.body ).on( 'click dragstart', '.evf-registered-item.enable-stripe-model', this.enable_stripe_model );
 			$( document.body ).on( 'click dragstart', '.evf-registered-item.enable-authorize-net-model', this.enable_authorize_net_model );
+			$( document.body ).on( 'click dragstart', '.evf-registered-item.enable-verifone-model', this.enable_verifone_model );
 			$( document.body ).on( 'click dragstart', '.everest-forms-field-option-row.upgrade-modal', this.feature_upgrade );
 			$( document.body ).on( 'click dragstart', '.evf-upgradable-feature, .everest-forms-btn-group span.upgrade-modal', this.feature_upgrade );
 			$( document.body ).on( 'click dragstart', '.evf-one-time-draggable-field, .evf-registered-item.evf-one-time-draggable-field', this.evf_one_time_draggable_field );
@@ -190,6 +191,22 @@ jQuery( function( $ ) {
 			$.alert({
 				title: evf_upgrade.enable_authorize_net_title,
 				content: evf_upgrade.enable_authorize_net_message,
+				icon: 'dashicons dashicons-info',
+				type: 'blue',
+				buttons : {
+					confirm : {
+						text: evf_data.i18n_close,
+						btnClass: 'btn-confirm',
+						keys: ['enter']
+					}
+				}
+			});
+		},
+		enable_verifone_model: function( e ) {
+			e.preventDefault();
+			$.alert({
+				title: evf_upgrade.enable_verifone_title,
+				content: evf_upgrade.enable_verifone_message,
 				icon: 'dashicons dashicons-info',
 				type: 'blue',
 				buttons : {
---
+++ includes/fields/class-evf-field-payment-verifone.php
@@ -0,0 +1,35 @@
+<?php
+/**
+ * Verifone field
+ *
+ * @package EverestForms\Fields
+ * @since   1.9.9
+ */
+
+defined( 'ABSPATH' ) || exit;
+
+/**
+ * EVF_Field_Payment_Verifone_Net Class.
+ */
+class EVF_Field_Payment_Verifone extends EVF_Form_Fields {
+
+	/**
+	 * Constructor.
+	 */
+	public function __construct() {
+		$this->name   = esc_html__( '(2Checkout)Verifone', 'everest-forms' );
+		$this->type   = 'verifone';
+		$this->icon   = 'evf-icon evf-icon-payment';
+		$this->order  = 232;
+		$this->group  = 'payment';
+		$this->is_pro = true;
+		$this->plan   = 'personal agency themegrill-agency';
+		$this->addon  = 'everest-forms-verifone';
+		$this->links  = array(
+			'image_id' => '',
+			'vedio_id' => '',
+		);
+
+		parent::__construct();
+	}
+}
