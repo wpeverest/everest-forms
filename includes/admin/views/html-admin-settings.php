ab970fb739bc1bc7a62ae239dd4b94b6ebe5657e
Deependra Chaudhary <elendoff11@gmail.com>
2024-06-27 14:08:19 +0545

    Added - COntent for promotionla content

--- includes/admin/views/html-admin-settings.php
+++ includes/admin/views/html-admin-settings.php
@@ -7,12 +7,17 @@
 
 defined( 'ABSPATH' ) || exit;
 
-$tab_exists        = isset( $tabs[ $current_tab ] ) || has_action( 'everest_forms_sections_' . $current_tab ) || has_action( 'everest_forms_settings_' . $current_tab );
-$current_tab_label = isset( $tabs[ $current_tab ] ) ? $tabs[ $current_tab ] : '';
+$tab_exists                 = isset( $tabs[ $current_tab ] ) || has_action( 'everest_forms_sections_' . $current_tab ) || has_action( 'everest_forms_settings_' . $current_tab );
+$current_tab_label          = isset( $tabs[ $current_tab ] ) ? $tabs[ $current_tab ] : '';
+$is_premium_sidebar_enabled = isset( $_COOKIE['isPremiumSidebarEnabled'] ) ? sanitize_text_field( wp_unslash( $_COOKIE['isPremiumSidebarEnabled'] ) ) : '';
+$is_premium_sidebar_class   = $is_premium_sidebar_enabled ? 'everest-forms-hidden' : '';
 if ( ! $tab_exists ) {
 	wp_safe_redirect( admin_url( 'admin.php?page=evf-settings' ) );
 	exit;
 }
+
+
+
 ?>
 <div class="wrap everest-forms">
 <?php if ( 'integration' !== $current_tab ) : ?>
@@ -63,6 +68,7 @@ if ( ! $tab_exists ) {
 
 			<div class="everest-forms-settings-container">
 				<div class="everest-forms-settings-main">
+				<?php if ( ! defined( 'EFP_PLUGIN_FILE' ) ) { ?>
 					<div class="everest-forms-toggle-wrapper">
 						<div class="evf-toggle-section">
 							<span class="everest-forms-toggle-form">
@@ -71,8 +77,8 @@ if ( ! $tab_exists ) {
 							</span>
 						</div>
 						<span class="everest-forms-toggle-text">Hide Sidebar</span>
-
 					</div>
+					<?php } ?>
 				<?php
 					do_action( 'everest_forms_sections_' . $current_tab );
 
@@ -90,39 +96,42 @@ if ( ! $tab_exists ) {
 
 		</div>
 		</div>
-
-		<div class="everest-forms-settings-premium-sidebar" id="everest-forms-settings-premium-sidebar">
+		<?php if ( ! defined( 'EFP_PLUGIN_FILE' ) ) { ?>
+		<div class="everest-forms-settings-premium-sidebar <?php echo esc_attr( $is_premium_sidebar_class ); ?>" id="everest-forms-settings-premium-sidebar">
 			<?php
-			$allowed_html = array(
-				'a'   => array(
-					'href'   => array(),
-					'target' => array(),
-				),
-				'h2'  => array(),
-				'p'   => array(),
-				'h3'  => array(),
-				'ul'  => array(),
-				'li'  => array(),
-				'!--' => array(),
-			);
+				$allowed_html = array(
+					'a'   => array(
+						'href'   => array(),
+						'target' => array(),
+					),
+					'h2'  => array(),
+					'p'   => array(),
+					'h3'  => array(),
+					'ul'  => array(),
+					'li'  => array(),
+					'!--' => array(),
+				);
 
-			$content = '<h2>Get Even More from Everest Forms with the Premium Plan</h2>
-			<p>The free version of Everest Forms is just the beginning of something special. With our Pro plugin, you can get everything your website needs in terms of form building and integration.</p>
-			<h3>Premium Benefits:</h3>
-			<ul>
-				<li>Custom registration forms with username and password</li>
-				<li>Growing library of pre-built form templates</li>
-				<!-- Add other benefits -->
-			</ul>
-			<p>To enjoy all the benefits of Everest Forms Pro...</p>
-			<a href="https://everestforms.net/pricing/" class="everest-forms-premium-sidebar-button">Get the Everest Forms Pro</a>
-			<p>Thank you for choosing Everest Forms 😊</p>';
-
-			echo wp_kses( $content, $allowed_html );
-			?>
-
-			</div>
-
+				$content = '<h2>Get Even More from Everest Forms with the Premium Plan</h2>
+				<p>The free version of Everest Forms is just the start. Upgrade to our Pro version for everything you need for advanced form building.</p>
+				<h3>Premium Benefits:</h3>
+				<ul>
+					<li>Instant access to 35+ unique addons </li>
+					<li>Attention grabbing forms with advanced style customizer </li>
+					<li>Form security with custom captcha</li>
+					<li>Multiple payment options (PayPal, Stripe, etc.)</li>
+					<li>Interactive multi-step forms, and quizzes</li>
+					<li>Tables and graphics in Entries</li>
+					<li>Priority support for premium users</li>
+				</ul>
+				<p>To enjoy all the benefits of Everest Forms Pro...</p>
+				<a href="https://everestforms.net/pricing/?utm_campaign=lite-version&utm_source=premium-cta-&utm_medium=upgrade-cta-button"  target="_blank" class="everest-forms-premium-sidebar-button">Get the Everest Forms Pro</a>
+				<p>Thank you for choosing Everest Forms 😊</p>';
+				$content = apply_filters( 'everest_forms_cta_promotiona_content', $content );
+				echo wp_kses( $content, $allowed_html );
+				?>
+		</div>
+		<?php } ?>
 	<?php if ( 'integration' !== $current_tab ) : ?>
 	</div>
 	</form>
