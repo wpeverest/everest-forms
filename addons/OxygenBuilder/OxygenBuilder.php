600e50f0b513c5335f966d5e8024859c7ba31cb0
MILAN88888 <chaudharymilan996@gmail.com>
2024-10-01 12:53:41 +0545

    Add - Basic setup for the oxygen builder

---
+++ addons/OxygenBuilder/OxygenBuilder.php
@@ -0,0 +1,23 @@
+<?php
+/**
+ * Oxygen builder integration.
+ *
+ * @since xx.xx.xx
+ * @package EverestForms\Addons\OxygenBuilder\OxygenBuilder
+ */
+namespace EverestForms\Addons\OxygenBuilder;
+
+use EverestForms\Traits\Singleton;
+
+/**
+ * OxygenBuilder.
+ *
+ * @since xx.xx.xx
+ */
+class OxygenBuilder {
+
+	use Singleton;
+
+	public function __construct() {
+	}
+}
