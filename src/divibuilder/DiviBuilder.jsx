d4d4ed8b1b75b8084259801a9b1c039a951d6334
Deependra Chaudhary <elendoff11@gmail.com>
2024-10-08 12:54:57 +0545

    Feature - Divi Builder compatibility

---
+++ src/divibuilder/DiviBuilder.jsx
@@ -0,0 +1,14 @@
+// External Dependencies
+import React, { Component} from 'react';
+
+class DiviBuilder extends Component {
+
+  static slug = 'everest_forms_divi_builder';
+  render() {
+    return (
+		<div className='everest-forms-divi-builder'></div>
+    );
+  }
+}
+
+export default DiviBuilder; 
