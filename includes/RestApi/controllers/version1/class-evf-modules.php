3989a35fdb14b857cae3a6ba71d91848e54b046c
Niraj Chaudhary <nirajchy2000@gmail.com>
2024-10-30 10:41:28 +0545

    Fix - Upgrade to pro issue in Bricks builder

--- includes/RestApi/controllers/version1/class-evf-modules.php
+++ includes/RestApi/controllers/version1/class-evf-modules.php
@@ -113,7 +113,7 @@ class EVF_Modules {
 			$feature->link          = $feature->link . '&utm_campaign=' . EVF()->utm_campaign;
 			$feature->type          = 'feature';
 			$features_lists[ $key ] = $feature;
-			if ( in_array( $feature->slug, array( 'everest-forms-oxygen-builder' ), true ) ) {
+			if ( in_array( $feature->slug, array( 'everest-forms-oxygen-builder', 'everest-forms-bricks-builder' ), true ) ) {
 				$feature->required_plan = esc_html__( 'Free', 'everest-forms' );
 			}
 		}
