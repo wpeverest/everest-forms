093fd7ef07c44a21ae16d094b074ab8311e06a38
Deependra Chaudhary <elendoff11@gmail.com>
2024-12-11 17:37:44 +0545

    Elementor popup issue fixed

--- assets/js/frontend/everest-forms.js
+++ assets/js/frontend/everest-forms.js
@@ -23,7 +23,11 @@ jQuery( function ( $ ) {
 		init: function() {
 			this.init_inputMask();
 			this.init_mailcheck();
+			$(document).on('elementor/popup/show', () => {
+				this.init_datepicker();
+			});
 			this.init_datepicker();
+			// this.init_datepicker();
 			this.init_datedropdown();
 			this.load_validation();
 			this.submission_scroll();
@@ -118,7 +122,6 @@ jQuery( function ( $ ) {
 		if (window.location.search.includes('ct_builder=true')) {
 			return;
 		}
-
 			var evfDateField = $( '.evf-field-date-time' );
 			if ( evfDateField.length && evfDateField.find( '.flatpickr-field' ).length ) {
 				evfDateField.find( '.flatpickr-field' ).each( function () {
@@ -213,44 +216,44 @@ jQuery( function ( $ ) {
 					}
 				} );
 			}
-				$(document).find(".evf-field-date-time input").on('change', function (event) {
-					var slotBooking = $(this).data('slot-booking'),
-						targetLabel = $(this).parent(),
-						errorLabel = $(this).parent().find('label.evf-error');
-					if(slotBooking === 1) {
-						var dataTimeValue = $(this).val(),
-						dateFormat = $(this).data('date-format'),
-						dateTimeFormat = $(this).data('date-time'),
-						mode = $(this).data('mode'),
-						form_id = $(this).data('form-id'),
-						time_interval = $(this).data('time-interval'),
-						data = {'action':'everest_forms_slot_booking', 'data-time-value':dataTimeValue, 'data-format': dateFormat, 'data-time-format': dateTimeFormat, 'mode': mode, 'form-id': form_id, 'time-interval':time_interval, 'security': everest_forms_params.everest_forms_slot_booking};
+				// $(document).find(".evf-field-date-time input").on('change', function (event) {
+				// 	var slotBooking = $(this).data('slot-booking'),
+				// 		targetLabel = $(this).parent(),
+				// 		errorLabel = $(this).parent().find('label.evf-error');
+				// 	if(slotBooking === 1) {
+				// 		var dataTimeValue = $(this).val(),
+				// 		dateFormat = $(this).data('date-format'),
+				// 		dateTimeFormat = $(this).data('date-time'),
+				// 		mode = $(this).data('mode'),
+				// 		form_id = $(this).data('form-id'),
+				// 		time_interval = $(this).data('time-interval'),
+				// 		data = {'action':'everest_forms_slot_booking', 'data-time-value':dataTimeValue, 'data-format': dateFormat, 'data-time-format': dateTimeFormat, 'mode': mode, 'form-id': form_id, 'time-interval':time_interval, 'security': everest_forms_params.everest_forms_slot_booking};
 
-						$.ajax({
-							url:everest_forms_params.ajax_url,
-							data: data,
-							type: 'POST',
-							beforeSend: function () {
-								var submitButton = $(document).find('.evf-submit-container button');
-								$(submitButton).prop('disabled', true);
-							},
-							success: function (res) {
-								if($(errorLabel).length) {
-									$(errorLabel).remove();
-								}
-								if(res.success === true) {
-									var message = res.data.message;
-									$(targetLabel).append('<label class="evf-error">'+message+'</label>');
-									var submitButton = $(document).find('.evf-submit-container button');
-									$(submitButton).prop('disabled', true);
-								} else {
-									var submitButton = $(document).find('.evf-submit-container button');
-									$(submitButton).prop('disabled', false);
-								}
-							}
-						});
-					}
-				})
+				// 		$.ajax({
+				// 			url:everest_forms_params.ajax_url,
+				// 			data: data,
+				// 			type: 'POST',
+				// 			beforeSend: function () {
+				// 				var submitButton = $(document).find('.evf-submit-container button');
+				// 				$(submitButton).prop('disabled', true);
+				// 			},
+				// 			success: function (res) {
+				// 				if($(errorLabel).length) {
+				// 					$(errorLabel).remove();
+				// 				}
+				// 				if(res.success === true) {
+				// 					var message = res.data.message;
+				// 					$(targetLabel).append('<label class="evf-error">'+message+'</label>');
+				// 					var submitButton = $(document).find('.evf-submit-container button');
+				// 					$(submitButton).prop('disabled', true);
+				// 				} else {
+				// 					var submitButton = $(document).find('.evf-submit-container button');
+				// 					$(submitButton).prop('disabled', false);
+				// 				}
+				// 			}
+				// 		});
+				// 	}
+				// })
 
 		},
 		init_datedropdown: function () {
