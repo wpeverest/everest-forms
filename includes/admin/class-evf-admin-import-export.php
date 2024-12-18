9683566e40638a4e02a39f612c66cf8cbbca7ba4
riteelama <riteelama@gmail.com>
2024-12-18 14:27:54 +0545

    Fix - Select all forms option

--- includes/admin/class-evf-admin-import-export.php
+++ includes/admin/class-evf-admin-import-export.php
@@ -44,7 +44,7 @@ class EVF_Admin_Import_Export {
 		$export_all_forms = array();
 		$file_name        = 'evf-export-forms-' . current_time( 'Y-m-d_H:i:s' ) . '.json';
 
-		if ( count( $form_ids ) === 1 && isset( $form_ids[0] ) && 'select_all_forms' === $form_ids[0] ) {
+		if ( isset( $form_ids[0] ) && 'select_all_forms' === $form_ids[0] ) {
 			$all_forms = evf_get_all_forms();
 
 			foreach ( $all_forms as $key => $form_id ) {
@@ -97,7 +97,6 @@ class EVF_Admin_Import_Export {
 				$export_all_forms[] = $export_data;
 			}
 		}
-
 		$forms['forms'] = $export_all_forms;
 
 		// Force download.
