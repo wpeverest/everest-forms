ce684520c5df513b5fd93fbbb44b71ea050b5d28
Niraj Chaudhary <nirajchy2000@gmail.com>
2025-01-07 09:13:07 +0545

    Fix - Delete popup design issue

--- src/rolesandpermission/components/TrashUserRoleModel.jsx
+++ src/rolesandpermission/components/TrashUserRoleModel.jsx
@@ -1,35 +1,46 @@
-import { Button, Modal, ModalBody, ModalContent, ModalFooter, ModalHeader, ModalOverlay, useDisclosure } from '@chakra-ui/react';
-import React from 'react'
+import { AlertDialog, AlertDialogBody, AlertDialogCloseButton, AlertDialogContent, AlertDialogFooter, AlertDialogHeader, AlertDialogOverlay, Button, Modal, ModalBody, ModalContent, ModalFooter, ModalHeader, ModalOverlay, useDisclosure } from '@chakra-ui/react';
+import React, { useRef } from 'react'
 
 const TrashUserRoleModel = ( { deleteManager } ) => {
   const { isOpen, onOpen, onClose } = useDisclosure();
+  const cancelRef = useRef()
   return (
     <>
-      <Button
-	 	style={{
-			color: "#475BB2",
-			fontWeight: "400",
-			fontSize: "13px",
-			backgroundColor: "#ffffff",
-			padding: 0,
-		}}
-	  	onClick={onOpen}>Trash</Button>
-
-      <Modal isOpen={isOpen} onClose={onClose} size={"lg"}>
-        <ModalOverlay />
-        <ModalContent>
-          <ModalBody>
-		  		Are you sure to delete this?
-          </ModalBody>
-
-          <ModalFooter>
-            <Button variantColor="blue" mr={3} onClick={onClose}>
-              Cancel
+	  	<Button
+	 		style={{
+				color: "#475BB2",
+				fontWeight: "400",
+				fontSize: "13px",
+				backgroundColor: "#ffffff",
+				padding: 0,
+			}}
+	  		onClick={onOpen}>
+				Trash
+		</Button>
+      <AlertDialog
+        motionPreset='slideInBottom'
+        leastDestructiveRef={cancelRef}
+        onClose={onClose}
+        isOpen={isOpen}
+        isCentered
+      >
+        <AlertDialogOverlay />
+        <AlertDialogContent>
+          <AlertDialogHeader>Delete Manager?</AlertDialogHeader>
+          <AlertDialogCloseButton />
+          <AlertDialogBody>
+            Are you sure you want to delete manager.
+          </AlertDialogBody>
+          <AlertDialogFooter>
+            <Button ref={cancelRef} onClick={onClose}>
+              No
             </Button>
-            <Button variant="ghost" onClick={deleteManager}>Confirm</Button>
-          </ModalFooter>
-        </ModalContent>
-      </Modal>
+            <Button colorScheme='red' ml={3} onClick={deleteManager}>
+              Yes
+            </Button>
+          </AlertDialogFooter>
+        </AlertDialogContent>
+      </AlertDialog>
     </>
   )
 
