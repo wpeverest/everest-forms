419ac6c02cc306f64a4cc583d93635b0d76f8e4f
Niraj Chaudhary <nirajchy2000@gmail.com>
2025-03-19 09:44:05 +0545

    Added - Settings, entries and form builder link in form preview

--- templates/form-preview/evf-form-preview-template.php
+++ templates/form-preview/evf-form-preview-template.php
@@ -39,7 +39,7 @@ wp_head();
 
 		</div>
 
-		<div class="major-publishing-actions wp-clearfix">
+	<div class="major-publishing-actions wp-clearfix">
 			<div class="publishing-action">
 				<input type="text" onfocus="this.select();" readonly="readonly"
 						value='[everest_form id="<?php echo esc_attr( $form_id ); ?>"]'
@@ -55,6 +55,19 @@ wp_head();
 </button>
 
 			</div>
+		</div>
+		<div class="evf-form-preview-dropdown-container">
+			<div id="evf-form-preview-more-option" style="margin-left: 16px; cursor: pointer;">
+				<svg width="40" height="38" viewBox="0 0 40 38" fill="none" xmlns="http://www.w3.org/2000/svg">
+					<rect width="40" height="38" rx="3" fill="#F6F3FA"/>
+					<path d="M18.0742 25.0062V24.5812C18.0836 24.5468 18.0992 24.5156 18.1055 24.4812C18.243 23.5875 18.9867 22.8812 19.8586 22.8218C20.8336 22.7531 21.6586 23.3093 21.9273 24.2156C21.9617 24.3343 21.9867 24.4593 22.018 24.5812V25.0062C22.0086 25.0343 21.993 25.0593 21.9898 25.0906C21.8555 25.9156 21.2617 26.5437 20.4492 26.7218C20.3867 26.7343 20.3242 26.7531 20.2617 26.7687H19.8367C19.8086 26.7593 19.7805 26.7437 19.7523 26.7406C18.9398 26.6093 18.3211 26.0312 18.1336 25.2281C18.1148 25.1499 18.093 25.0781 18.0742 25.0062ZM22.018 12.9937L22.018 13.4188C22.0086 13.4531 21.993 13.4844 21.9898 13.5188C21.8492 14.4188 21.1023 15.1219 20.2211 15.1781C19.2492 15.2406 18.4273 14.6781 18.1617 13.7688C18.1273 13.6531 18.1023 13.5344 18.0742 13.4188V12.9937C18.0836 12.9656 18.0992 12.9406 18.1055 12.9125C18.2555 12.1 18.7273 11.5688 19.5148 11.3156C19.618 11.2813 19.7273 11.2625 19.8336 11.2344H20.2586C20.2867 11.2438 20.3148 11.2594 20.343 11.2625C21.1586 11.3969 21.7742 11.9719 21.9617 12.775C21.9805 12.85 21.9992 12.9219 22.018 12.9937ZM22.018 18.7875V19.2125C22.0086 19.2469 21.993 19.2781 21.9867 19.3125C21.8461 20.2094 21.0711 20.9312 20.1961 20.9688C19.2117 21.0125 18.3773 20.4187 18.143 19.5031C18.118 19.4062 18.0961 19.3094 18.0742 19.2125V18.7875C18.0836 18.7531 18.0992 18.7219 18.1055 18.6875C18.243 17.7906 19.0211 17.0687 19.8961 17.0281C20.8805 16.9812 21.7148 17.5781 21.9492 18.4938C21.9742 18.5938 21.9961 18.6906 22.018 18.7875Z" fill="#383838"/>
+				</svg>
+			</div>
+			<ul class="evf-form-preview-dropdown-content">
+				<li><a target="_blank" href="<?php echo admin_url( 'admin.php?page=evf-builder&tab=fields&form_id=' . $_GET['form_id'] ); ?>">Form Builder</a></li>
+				<li><a target="_blank" href="<?php echo admin_url( 'admin.php?page=evf-builder&tab=settings&form_id=' . $_GET['form_id'] ); ?>">Form Settings</a></li>
+				<li><a target="_blank" href="<?php echo admin_url( 'admin.php?page=evf-entries&form_id=' . $_GET['form_id'] ); ?>">Form Entries</a></li>
+			</ul>
 		</div>
 	</div>
 	<svg class="evf-form-preview-sidepanel-toggler" xmlns="http://www.w3.org/2000/svg" viewBox="0 0 8 12">
