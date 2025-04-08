925328ae6bc7ef93f2c699f521b6d5a6cda1e089
Ritee Lama <89212873+riteelama@users.noreply.github.com>
2025-04-08 15:51:35 +0545

    Fix - Critical error while viewing entries. (#1417)
    * Fix - Critical error while ciewing entries
    
    * Fix - Optimized the entry list
    
    * Added - Isset condition
    
    ---------
    
    Co-authored-by: Niraj Chaudhary <nirajchy2000@gmail.com>

--- includes/admin/class-evf-admin-entries-table-list.php
+++ includes/admin/class-evf-admin-entries-table-list.php
@@ -256,12 +256,14 @@ class EVF_Admin_Entries_Table_List extends WP_List_Table {
 				}
 			}
 
-			// Limit to 5 lines.
+			$value = isset( $value['label'] ) && is_array( $value['label'] ) ? implode( ', ', $value['label'] ) : $value;
+
 			if ( false === strpos( $value, 'http' ) ) {
 				$lines = explode( "\n", $value );
 				$value = array_slice( $lines, 0, 4 );
 				$value = implode( "\n", $value );
 
+				// limit 5 lines.
 				if ( count( $lines ) > 5 ) {
 					$value .= '&hellip;';
 				} elseif ( strlen( $value ) > 75 ) {
@@ -270,6 +272,7 @@ class EVF_Admin_Entries_Table_List extends WP_List_Table {
 
 				$value = nl2br( wp_strip_all_tags( trim( $value ) ) );
 			}
+
 			return apply_filters( 'everest_forms_html_field_value', $value, $entry->meta[ $meta_key ], $entry, 'entry-table', $meta_key );
 		} else {
 			return '<span class="na">&mdash;</span>';
