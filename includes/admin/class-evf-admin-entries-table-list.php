1e2e371aa5bdc3d5977e0bf45abe8890f1698c70
Niraj Chaudhary <nirajchy2000@gmail.com>
2025-06-04 13:08:03 +0545

    Added - Placeholder on entry form table

--- includes/admin/class-evf-admin-entries-table-list.php
+++ includes/admin/class-evf-admin-entries-table-list.php
@@ -785,7 +785,7 @@ class EVF_Admin_Entries_Table_List extends WP_List_Table {
 		$form_id = isset( $_REQUEST['form_id'] ) ? absint( $_REQUEST['form_id'] ) : $this->form_id; // phpcs:ignore WordPress.Security.NonceVerification
 		?>
 		<label for="filter-by-form" class="screen-reader-text"><?php esc_html_e( 'Filter by form', 'everest-forms' ); ?></label>
-		<select name="form_id" id="filter-by-form" class="evf-enhanced-normal-select" style="min-width: 200px;">
+		<select name="form_id" id="filter-by-form" class="evf-enhanced-normal-select" style="min-width: 200px;" data-placeholder="<?php esc_attr_e( 'Search form...', 'everest-forms' ); ?>">
 			<?php foreach ( $forms as $id => $form ) : ?>
 				<option value="<?php echo esc_attr( $id ); ?>" <?php selected( $form_id, $id ); ?>><?php echo esc_html( $form ); ?></option>
 			<?php endforeach; ?>
