985e0864f6956a27bb5da07e33253bfe5a0450bd
Deependra Chaudhary <elendoff11@gmail.com>
2026-03-04 16:43:46 +0545

    Moved entries reports

--- includes/admin/builder/class-evf-builder-settings.php
+++ includes/admin/builder/class-evf-builder-settings.php
@@ -92,7 +92,7 @@ class EVF_Builder_Settings extends EVF_Builder_Page
 
 ?>
 		<div class="everest-forms-active-email">
-			<button class="everest-forms-btn everest-forms-btn-primary everest-forms-email-add" data-form_id="<?php echo isset($_GET['form_id']) ? absint(sanitize_text_field(wp_unslash($_GET['form_id']))) : 0; // phpcs:ignore WordPress.Security.NonceVerification 
+			<button class="everest-forms-btn everest-forms-btn-primary everest-forms-email-add" data-form_id="<?php echo isset($_GET['form_id']) ? absint(sanitize_text_field(wp_unslash($_GET['form_id']))) : 0; // phpcs:ignore WordPress.Security.NonceVerification
 																												?>" data-source="email" data-type="<?php echo esc_attr('connection'); ?>">
 				<svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 24 24">
 					<path d="M12 21.95c-.6 0-1-.4-1-1v-8H3.1c-.6 0-1-.4-1-1s.4-1 1-1H11v-7.9c0-.6.4-1 1-1s1 .4 1 1v7.9h7.9c.6 0 1 .4 1 1s-.4 1-1 1H13v8c0 .6-.4 1-1 1Z" />
@@ -154,23 +154,9 @@ class EVF_Builder_Settings extends EVF_Builder_Page
 									?>
 									<a class="<?php echo esc_attr($preview_class); ?>" target="__blank" data-connection-id="<?php echo esc_attr($connection_id); ?>" href="<?php echo esc_url($preview_url); ?>">
 										<span class="<?php echo esc_attr($preview_class); ?>">
-											<svg xmlns="http://www.w3.org/2000/svg" xmlns="http://www.w3.org/1999/svg"
-												viewBox="0 0 442.04 442.04" xml:space="preserve">
-												<g id="SVGRepo_bgCarrier" stroke-width="0"></g>
-												<g id="SVGRepo_tracerCarrier" stroke-linecap="round" stroke-linejoin="round"></g>
-												<g id="SVGRepo_iconCarrier">
-													<g>
-														<g>
-															<path d="M221.02,341.304c-49.708,0-103.206-19.44-154.71-56.22C27.808,257.59,4.044,230.351,3.051,229.203 c-4.068-4.697-4.068-11.669,0-16.367c0.993-1.146,24.756-28.387,63.259-55.881c51.505-36.777,105.003-56.219,154.71-56.219 c49.708,0,103.207,19.441,154.71,56.219c38.502,27.494,62.266,54.734,63.259,55.881c4.068,4.697,4.068,11.669,0,16.367 c-0.993,1.146-24.756,28.387-63.259,55.881C324.227,321.863,270.729,341.304,221.02,341.304z M29.638,221.021 c9.61,9.799,27.747,27.03,51.694,44.071c32.83,23.361,83.714,51.212,139.688,51.212s106.859-27.851,139.688-51.212 c23.944-17.038,42.082-34.271,51.694-44.071c-9.609-9.799-27.747-27.03-51.694-44.071 c-32.829-23.362-83.714-51.212-139.688-51.212s-106.858,27.85-139.688,51.212C57.388,193.988,39.25,211.219,29.638,221.021z"></path>
-														</g>
-														<g>
-															<path d="M221.02,298.521c-42.734,0-77.5-34.767-77.5-77.5c0-42.733,34.766-77.5,77.5-77.5c18.794,0,36.924,6.814,51.048,19.188 c5.193,4.549,5.715,12.446,1.166,17.639c-4.549,5.193-12.447,5.714-17.639,1.166c-9.564-8.379-21.844-12.993-34.576-12.993 c-28.949,0-52.5,23.552-52.5,52.5s23.551,52.5,52.5,52.5c28.95,0,52.5-23.552,52.5-52.5c0-6.903,5.597-12.5,12.5-12.5 s12.5,5.597,12.5,12.5C298.521,263.754,263.754,298.521,221.02,298.521z"></path>
-														</g>
-														<g>
-															<path d="M221.02,246.021c-13.785,0-25-11.215-25-25s11.215-25,25-25c13.786,0,25,11.215,25,25S234.806,246.021,221.02,246.021z"></path>
-														</g>
-													</g>
-												</g>
+											<svg xmlns="http://www.w3.org/2000/svg" fill="currentColor" viewBox="0 0 24 24">
+											<path d="M12 5c2.116 0 4.183.648 5.941 1.861a10.94 10.94 0 0 1 3.776 4.58l.158.375.011.032a1.93 1.93 0 0 1-.011 1.335 10.95 10.95 0 0 1-3.934 4.956A10.45 10.45 0 0 1 12.001 20c-2.116 0-4.184-.648-5.942-1.861a10.95 10.95 0 0 1-3.934-4.955l-.011-.032a1.93 1.93 0 0 1 0-1.304l.011-.032A10.95 10.95 0 0 1 6.06 6.861 10.45 10.45 0 0 1 12 5m0 1.875A8.7 8.7 0 0 0 7.07 8.42a9.1 9.1 0 0 0-3.251 4.08 9.1 9.1 0 0 0 3.25 4.08A8.7 8.7 0 0 0 12 18.125a8.67 8.67 0 0 0 4.93-1.545c1.45-1 2.58-2.42 3.25-4.08a9.1 9.1 0 0 0-3.25-4.08A8.67 8.67 0 0 0 12 6.875"/>
+											<path d="M13.818 12.5c0-1.036-.814-1.875-1.818-1.875s-1.818.84-1.818 1.875.814 1.875 1.818 1.875 1.818-.84 1.818-1.875m1.818 0c0 2.071-1.628 3.75-3.636 3.75s-3.636-1.679-3.636-3.75S9.992 8.75 12 8.75s3.636 1.679 3.636 3.75"/>
 											</svg>
 											<a href="#" class="everest-forms-email-duplicate">
 												<span class="<?php echo esc_attr($duplicate_class); ?>">
--- includes/admin/settings/class-evf-settings-email.php
+++ includes/admin/settings/class-evf-settings-email.php
@@ -22,23 +22,9 @@ class EVF_Settings_Email extends EVF_Settings_Page {
 	 */
 	public function __construct() {
 		$this->id    = 'email';
-		$this->label = esc_html__( 'Email & Reports', 'everest-forms' );
+		$this->label = esc_html__( 'Email', 'everest-forms' );
 
 		parent::__construct();
-	}
-
-	/**
-	 * Get sections for email and reports tab.
-	 *
-	 * @return array
-	 */
-	public function get_sections() {
-		$sections = array(
-			'general'       => esc_html__( 'General', 'everest-forms' ),
-			'entry_reports' => esc_html__( 'Entry Reports & Summaries', 'everest-forms' ),
-		);
-
-		return apply_filters( 'everest_forms_get_sections_' . $this->id, $sections );
 	}
 
 	/**
@@ -46,297 +32,64 @@ class EVF_Settings_Email extends EVF_Settings_Page {
 	 *
 	 * @return array
 	 */
-	// public function get_settings() {
-	// $settings = apply_filters(
-	// 'everest_forms_email_settings',
-	// array(
-	// array(
-	// 'title' => esc_html__( 'Template Settings', 'everest-forms' ),
-	// 'type'  => 'title',
-	// 'desc'  => '',
-	// 'id'    => 'email_template_options',
-	// ),
-	// array(
-	// 'title'   => esc_html__( 'Template', 'everest-forms' ),
-	// 'type'    => 'radio-image',
-	// 'id'      => 'everest_forms_email_template',
-	// 'desc'    => esc_html__( 'Determine which format of email to send. HTML Template is default.', 'everest-forms' ),
-	// 'default' => 'default',
-	// 'options' => array(
-	// 'default' => array(
-	// 'name'  => esc_html__( 'HTML Template', 'everest-forms' ),
-	// 'image' => plugins_url( 'assets/images/email-template-html.png', EVF_PLUGIN_FILE ),
-	// ),
-	// 'none'    => array(
-	// 'name'  => esc_html__( 'Plain text', 'everest-forms' ),
-	// 'image' => plugins_url( 'assets/images/email-template-plain.png', EVF_PLUGIN_FILE ),
-	// ),
-	// ),
-	// ),
-	// array(
-	// 'title'    => esc_html__( 'Enable copies', 'everest-forms' ),
-	// 'desc'     => esc_html__( 'Email addresses for Cc and Bcc can be applied from the form notification settings.', 'everest-forms' ),
-	// 'id'       => 'everest_forms_enable_email_copies',
-	// 'default'  => 'no',
-	// 'type'     => 'toggle',
-	// 'desc_tip' => true,
-	// ),
-	// array(
-	// 'id'          => 'everest_forms_email_send_to',
-	// 'title'       => esc_html__( 'Send Test Email To', 'everest-forms' ),
-	// 'desc'        => esc_html__( 'Enter the email address where test email will be sent.', 'everest-forms' ),
-	// 'input_id'    => 'everest_forms_email_send_to',
-	// 'placeholder' => 'eg. testemail@gmail.com',
-	// 'input_type'  => 'email',
-	// 'default'     => get_option( 'everest_forms_email_send_to', '' ) ? esc_attr( get_option( 'everest_forms_email_send_to', '' ) ) : esc_attr( get_bloginfo( 'admin_email' ) ),
-	// 'button_id'   => 'everest_forms_email_test',
-	// 'type'        => 'input_test_button',
-	// 'input_css'   => 'margin-right:0.5rem',
-	// 'buttons'     => array(
-	// array(
-	// 'title' => __( 'Send Test Email', 'everest-forms' ),
-	// 'href'  => 'javascript:;',
-	// 'class' => 'everest_forms_send_email_test',
-	// ),
-	// ),
-	// 'desc_tip'    => true,
-	// ),
-	// array(
-	// 'type' => 'sectionend',
-	// 'id'   => 'email_template_options',
-	// ),
-	// )
-	// );
-
-	// return apply_filters( 'everest_forms_get_settings_' . $this->id, $settings );
-	// }
-
-
-	/**
-	 * Output sections in navigation sidebar.
-	 */
-	public function output_sections() {
-		global $current_section;
-
-		$sections = $this->get_sections();
-
-		if ( empty( $sections ) || 1 === count( $sections ) ) {
-			return;
-		}
-
-		$current_section = isset( $_GET['section'] ) ? sanitize_text_field( wp_unslash( $_GET['section'] ) ) : 'general';
-
-		echo '<ul class="evf-subsections">';
-
-		foreach ( $sections as $id => $label ) {
-			$url = add_query_arg(
-				array(
-					'page'    => 'evf-settings',
-					'tab'     => $this->id,
-					'section' => sanitize_title( $id ),
-				),
-				admin_url( 'admin.php' )
-			);
-
-			echo '<li><a href="' . esc_url( $url ) . '" class="' . ( $current_section === $id ? 'current' : '' ) . '">' . esc_html( $label ) . '</a></li>';
-		}
-
-		echo '</ul>';
-	}
-
-	/**
-	 * Get settings array based on current section.
-	 *
-	 * @return array
-	 */
 	public function get_settings() {
-		global $current_section;
-
-		$current_section = isset( $_GET['section'] ) ? sanitize_text_field( wp_unslash( $_GET['section'] ) ) : 'general';
-
-		switch ( $current_section ) {
-			case 'entry_reports':
-				$settings = $this->get_entry_reports_settings();
-				break;
-			default:
-				$settings = $this->get_email_default_settings();
-				break;
-		}
-
-		return apply_filters( 'everest_forms_get_settings_' . $this->id, $settings, $current_section );
-	}
-
-
-	/**
-	 * Get email default settings.
-	 *
-	 * @return array
-	 */
-	public function get_email_default_settings() {
-			$settings = apply_filters(
-				'everest_forms_email_settings',
-				array(
-					array(
-						'title' => esc_html__( 'Template Settings', 'everest-forms' ),
-						'type'  => 'title',
-						'desc'  => '',
-						'id'    => 'email_template_options',
-					),
-					array(
-						'title'   => esc_html__( 'Template', 'everest-forms' ),
-						'type'    => 'radio-image',
-						'id'      => 'everest_forms_email_template',
-						'desc'    => esc_html__( 'Determine which format of email to send. HTML Template is default.', 'everest-forms' ),
-						'default' => 'default',
-						'options' => array(
-							'default' => array(
-								'name'  => esc_html__( 'HTML Template', 'everest-forms' ),
-								'image' => plugins_url( 'assets/images/email-template-html.png', EVF_PLUGIN_FILE ),
-							),
-							'none'    => array(
-								'name'  => esc_html__( 'Plain text', 'everest-forms' ),
-								'image' => plugins_url( 'assets/images/email-template-plain.png', EVF_PLUGIN_FILE ),
-							),
-						),
-					),
-					array(
-						'title'    => esc_html__( 'Enable copies', 'everest-forms' ),
-						'desc'     => esc_html__( 'Email addresses for Cc and Bcc can be applied from the form notification settings.', 'everest-forms' ),
-						'id'       => 'everest_forms_enable_email_copies',
-						'default'  => 'no',
-						'type'     => 'toggle',
-						'desc_tip' => true,
-					),
-					array(
-						'id'          => 'everest_forms_email_send_to',
-						'title'       => esc_html__( 'Send Test Email To', 'everest-forms' ),
-						'desc'        => esc_html__( 'Enter the email address where test email will be sent.', 'everest-forms' ),
-						'input_id'    => 'everest_forms_email_send_to',
-						'placeholder' => 'eg. testemail@gmail.com',
-						'input_type'  => 'email',
-						'default'     => get_option( 'everest_forms_email_send_to', '' ) ? esc_attr( get_option( 'everest_forms_email_send_to', '' ) ) : esc_attr( get_bloginfo( 'admin_email' ) ),
-						'button_id'   => 'everest_forms_email_test',
-						'type'        => 'input_test_button',
-						'input_css'   => 'margin-right:0.5rem',
-						'buttons'     => array(
-							array(
-								'title' => __( 'Send Test Email', 'everest-forms' ),
-								'href'  => 'javascript:;',
-								'class' => 'everest_forms_send_email_test',
-							),
-						),
-						'desc_tip'    => true,
-					),
-					array(
-						'type' => 'sectionend',
-						'id'   => 'email_template_options',
-					),
-				)
-			);
-
-		return apply_filters( 'everest_forms_get_settings_' . $this->id, $settings );
-	}
-
-	/**
-	 * Get entry reports settings.
-	 *
-	 * @return array
-	 */
-	public function get_entry_reports_settings() {
-			$evf_form_lists = evf_get_all_forms();
-		$evf_summary_email  = get_option( 'everest_forms_email_send_to', '' );
-		$settings           = apply_filters(
-			'everest_forms_reporting_settings',
+		$settings = apply_filters(
+			'everest_forms_email_settings',
 			array(
 				array(
-					'title' => esc_html__( 'Forms Entries Statistics Reporting', 'everest-forms' ),
+					'title' => esc_html__( 'Template Settings', 'everest-forms' ),
 					'type'  => 'title',
 					'desc'  => '',
-					'id'    => 'reporting_options',
+					'id'    => 'email_template_options',
 				),
 				array(
-					'title'    => esc_html__( 'Enable Entries Statistics Reporting', 'everest-forms' ),
-					'desc'     => esc_html__( 'Enable to send the entries statistics reporting email on routine basis.', 'everest-forms' ),
-					'id'       => 'everest_forms_enable_entries_reporting',
+					'title'   => esc_html__( 'Template', 'everest-forms' ),
+					'type'    => 'radio-image',
+					'id'      => 'everest_forms_email_template',
+					'desc'    => esc_html__( 'Determine which format of email to send. HTML Template is default.', 'everest-forms' ),
+					'default' => 'default',
+					'options' => array(
+						'default' => array(
+							'name'  => esc_html__( 'HTML Template', 'everest-forms' ),
+							'image' => plugins_url( 'assets/images/email-template-html.png', EVF_PLUGIN_FILE ),
+						),
+						'none'    => array(
+							'name'  => esc_html__( 'Plain text', 'everest-forms' ),
+							'image' => plugins_url( 'assets/images/email-template-plain.png', EVF_PLUGIN_FILE ),
+						),
+					),
+				),
+				array(
+					'title'    => esc_html__( 'Enable copies', 'everest-forms' ),
+					'desc'     => esc_html__( 'Email addresses for Cc and Bcc can be applied from the form notification settings.', 'everest-forms' ),
+					'id'       => 'everest_forms_enable_email_copies',
 					'default'  => 'no',
 					'type'     => 'toggle',
 					'desc_tip' => true,
 				),
 				array(
-					'title'    => esc_html__( 'Report Frequency', 'everest-forms' ),
-					'type'     => 'select',
-					'options'  => array(
-						'Daily'   => esc_html__( 'Daily', 'everest-forms' ),
-						'Weekly'  => esc_html__( 'Weekly', 'everest-forms' ),
-						'Monthly' => esc_html__( 'Monthly', 'everest-forms' ),
-					),
-					'id'       => 'everest_forms_entries_reporting_frequency',
-					'default'  => esc_html__( 'Weekly', 'everest-forms' ),
-					'desc'     => esc_html__( 'How often should the report be emailed?', 'everest-forms' ),
-					'desc_tip' => true,
-				),
-				array(
-					'title'    => esc_html__( 'Day To Send', 'everest-forms' ),
-					'type'     => 'select',
-					'options'  => array(
-						'sunday'    => esc_html__( 'Sunday', 'everest-forms' ),
-						'monday'    => esc_html__( 'Monday', 'everest-forms' ),
-						'tuesday'   => esc_html__( 'Tuesday', 'everest-forms' ),
-						'wednesday' => esc_html__( 'Wednesday', 'everest-forms' ),
-						'thursday'  => esc_html__( 'Thursday', 'everest-forms' ),
-						'friday'    => esc_html__( 'Friday', 'everest-forms' ),
-						'saturday'  => esc_html__( 'Saturday', 'everest-forms' ),
-					),
-					'id'       => 'everest_forms_entries_reporting_day',
-					'default'  => esc_html__( 'Monday', 'everest-forms' ),
-					'desc'     => esc_html__( 'What day of the week should the weekly report be sent?', 'everest-forms' ),
-					'desc_tip' => true,
-				),
-				array(
-					'title'    => esc_html__( 'Email To', 'everest-forms' ),
-					'desc_tip' => esc_html__( 'Email address to send the routine report', 'everest-forms' ),
-					'id'       => 'everest_forms_entries_reporting_email',
-					'default'  => '{admin_email}',
-					'type'     => 'text',
-				),
-				array(
-					'title'    => esc_html__( 'Email Subject', 'everest-forms' ),
-					'desc_tip' => esc_html__( 'Email subject while sending the routine report', 'everest-forms' ),
-					'id'       => 'everest_forms_entries_reporting_subject',
-					'default'  => esc_html__( 'Everest Forms - Entries summary statistics', 'everest-forms' ),
-					'type'     => 'text',
-				),
-				array(
-					'title'       => esc_html__( 'Send Test Report', 'everest-forms' ),
-					'desc'        => esc_html__( 'Enter the email address to receive the test email for the routine summary report.', 'everest-forms' ),
+					'id'          => 'everest_forms_email_send_to',
+					'title'       => esc_html__( 'Send Test Email To', 'everest-forms' ),
+					'desc'        => esc_html__( 'Enter the email address where test email will be sent.', 'everest-forms' ),
 					'input_id'    => 'everest_forms_email_send_to',
-					'input_type'  => 'email',
-					'input_css'   => 'margin-right:0.5rem',
 					'placeholder' => 'eg. testemail@gmail.com',
-					'value'       => ! empty( $evf_summary_email ) ? esc_attr( $evf_summary_email ) : esc_attr( get_bloginfo( 'admin_email' ) ),
-					'button_id'   => 'everest_forms_send_routine_report_test_email',
+					'input_type'  => 'email',
+					'default'     => get_option( 'everest_forms_email_send_to', '' ) ? esc_attr( get_option( 'everest_forms_email_send_to', '' ) ) : esc_attr( get_bloginfo( 'admin_email' ) ),
+					'button_id'   => 'everest_forms_email_test',
 					'type'        => 'input_test_button',
+					'input_css'   => 'margin-right:0.5rem',
 					'buttons'     => array(
 						array(
 							'title' => __( 'Send Test Email', 'everest-forms' ),
 							'href'  => 'javascript:;',
-							'class' => 'everest_forms_send_routine_report_test_email',
+							'class' => 'everest_forms_send_email_test',
 						),
 					),
 					'desc_tip'    => true,
 				),
 				array(
-					'title'    => esc_html__( 'Report Form Lists', 'everest-forms' ),
-					'id'       => 'everest_forms_reporting_form_lists',
-					'desc'     => esc_html__( 'Name of the forms to send the weekly report', 'everest-forms' ),
-					'desc_tip' => true,
-					'type'     => 'multiselect',
-					'options'  => ! empty( $evf_form_lists ) ? $evf_form_lists : array(),
-					'class'    => 'evf-enhanced-select',
-				),
-				array(
 					'type' => 'sectionend',
-					'id'   => 'reporting_options',
+					'id'   => 'email_template_options',
 				),
 			)
 		);
