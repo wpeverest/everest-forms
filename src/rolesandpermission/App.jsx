d95d7c5b18129ba37a3310809b043f1ae2a28a65
Rajat Gautam <83450756+rajatgautam755421@users.noreply.github.com>
2026-03-09 13:14:15 +0545

    Enhancement - Table UI Consistency  (#1518)
    * Minor scss fixes
    
    * Entries table Ui issue fixed
    
    * User roles and permissions UI updates
    
    * User roles and permissions UI updates
    
    * UI Updates
    
    * Add user modal updated in Roles and permissions table
    
    * Name col width increased in roles and permissions table

--- src/rolesandpermission/App.jsx
+++ src/rolesandpermission/App.jsx
@@ -1,13 +1,24 @@
-import React from "react";
-import { ChakraProvider, Stack } from "@chakra-ui/react";
-import UserRoleTable from "./components/UserRoleTable";
+import React from 'react';
+import { ChakraProvider, Stack } from '@chakra-ui/react';
+import { QueryClient, QueryClientProvider } from '@tanstack/react-query';
+import Theme from '../dashboard/Theme/Theme';
+import UserRoleTable from './components/UserRoleTable';
+
+const queryClient = new QueryClient();
 
 const App = () => {
 	return (
-		<ChakraProvider>
-			<Stack padding={"28px 32px"} backgroundColor={"#FFFFFF"} borderRadius={"7px"} direction={"column"}>
-				<UserRoleTable/>
-			</Stack>
+		<ChakraProvider theme={Theme}>
+			<QueryClientProvider client={queryClient}>
+				<Stack
+					direction="column"
+					gap="16px"
+					padding="20px 20px 40px 20px"
+					bg="white"
+				>
+					<UserRoleTable />
+				</Stack>
+			</QueryClientProvider>
 		</ChakraProvider>
 	);
 };
