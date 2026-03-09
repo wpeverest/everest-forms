975d26f29136cb163b30cb365893a1b2a58f2d33
Deependra Chaudhary <elendoff11@gmail.com>
2026-03-09 12:49:02 +0545

    Fix - Notice Consistency (#1519)
    * Fix:Notice issue
    
    * Refactor - Everest Forms Global Settings (#1521)
    
    * Rearranged:Free global settings
    
    * Fix:Integration setting
    
    * integration settings design fixed
    
    * Fix:Integration setting
    
    * fix:collapse issue in accordion
    
    * fix:Google Services - Both verify access code and authenticate button visible at same time
    
    * Removed Cleantalk from integration
    
    * Fix: Clean talk issue
    
    * Fix:ordering
    
    * Fix:Connected not showing issue and gap issue
    
    * Fix:Integration issue
    
    * Fix:Integration issue
    
    * Fix:Integration issue
    
    * Fix:Integration issue
    
    * Fix:Arrangment issue
    
    * Fix:Icon issue
    
    * Added:utilities
    
    * Moved entries reports
    
    * Fix:Blank issue in secuirty
    
    * Fix:Setting issue
    
    * Fix:Setting issue
    
    * Added:utility
    
    * Fix:Pro upsell showing issue
    
    * Fix:Pro upsell showing issue
    
    * settings, builder area design fixed
    
    * Fix:Link broken of clever reach
    
    * settings design fixed
    
    * settings page validation messages removed and footer design fixed
    
    * Fix:utilities hsowing issue
    
    * Fix:utilities sowing menu issue
    
    * Fix:labguage issue
    
    * Added:changelog
    
    ---------
    
    Co-authored-by: Ayush Shrestha <ayushstha143@gmail.com>
    
    * Enhancement - Form Builder.  (#1520)
    
    * Fix:Builder issue
    
    * Updated:changelog
    
    * chagelog updated
    
    ---------
    
    Co-authored-by: Ayush Shrestha <ayushstha143@gmail.com>

--- includes/admin/views/html-notice-updated.php
+++ includes/admin/views/html-notice-updated.php
@@ -8,7 +8,7 @@
 defined( 'ABSPATH' ) || exit;
 
 ?>
-<div id="message" class="updated everest-forms-message evf-connect everest-forms-message--success">
+<div id="message" class="updated everest-forms-message evf-connect everest-forms-message--success inline">
 	<a class="everest-forms-message-close notice-dismiss" href="<?php echo esc_url( wp_nonce_url( add_query_arg( 'evf-hide-notice', 'update', remove_query_arg( 'do_update_everest_forms' ) ), 'everest_forms_hide_notices_nonce', '_evf_notice_nonce' ) ); ?>"><?php esc_html_e( 'Dismiss', 'everest-forms' ); ?></a>
 
 	<p><?php esc_html_e( 'Everest Forms data update complete. Thank you for updating to the latest version!', 'everest-forms' ); ?></p>
