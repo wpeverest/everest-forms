630d616658b39699cbf45c9b046ce5bf1639e092
Deependra Chaudhary <elendoff11@gmail.com>
2026-04-02 10:23:43 +0545

    Fix:Style enqueque in other area

--- includes/admin/class-evf-admin-assets.php
+++ includes/admin/class-evf-admin-assets.php
@@ -45,15 +45,17 @@ class EVF_Admin_Assets {
 		wp_style_add_data( 'everest-forms-admin', 'rtl', 'replace' );
 		wp_style_add_data( 'everest-forms-admin-menu', 'rtl', 'replace' );
 
-		// Show hint in codemirror.
-		wp_enqueue_style( 'wp-codemirror' );
-		wp_enqueue_style( 'codemirror-hint-css', evf()->plugin_url() . '/assets/css/code-mirror/show-hint.min.css', array( 'wp-codemirror' ), EVF_VERSION );
+
 
 		// Sitewide menu CSS.
-		wp_enqueue_style( 'everest-forms-admin-menu' );
+
 
 		// Admin styles for EVF pages only.
 		if ( in_array( $screen_id, evf_get_screen_ids(), true ) ) {
+				// Show hint in codemirror.
+			wp_enqueue_style( 'wp-codemirror' );
+			wp_enqueue_style( 'codemirror-hint-css', evf()->plugin_url() . '/assets/css/code-mirror/show-hint.min.css', array( 'wp-codemirror' ), EVF_VERSION );
+			wp_enqueue_style( 'everest-forms-admin-menu' );
 			wp_enqueue_style( 'everest-forms-admin' );
 			wp_enqueue_style( 'jquery-confirm' );
 			wp_enqueue_style( 'jquery-ui-style' );
@@ -485,6 +487,11 @@ class EVF_Admin_Assets {
 	 * Enqueue dashboard scripts (React).
 	 */
 	public function enqueue_dashboard_scripts() {
+		$screen_id = get_current_screen()->id;
+		if ( ! in_array( $screen_id, evf_get_screen_ids(), true ) ) {
+			return;
+		}
+
 		// phpcs:ignore WordPress.Security.NonceVerification.Recommended
 		$current_page = isset( $_GET['page'] ) ? sanitize_text_field( wp_unslash( $_GET['page'] ) ) : '';
 		// phpcs:ignore WordPress.Security.NonceVerification.Recommended
