2173504bd9a4c14d3f6718c4757dfdd35351e314
Niraj Chaudhary <nirajchy2000@gmail.com>
2026-04-15 09:58:49 +0545

    added: d.m.Y date format

--- includes/fields/class-evf-field-date-time.php
+++ includes/fields/class-evf-field-date-time.php
@@ -183,6 +183,7 @@ class EVF_Field_Date_Time extends EVF_Form_Fields {
 				'F,Y'    => date_i18n( 'F,Y' ) . ' (F,Y)',
 				'm.d.y'  => date_i18n( 'm.d.y' ) . ' (m.d.y)',
 				'd.m.y'  => date_i18n( 'd.m.y' ) . ' (d.m.y)',
+				'd.m.y'  => date_i18n( 'd.m.Y' ) . ' (d.m.Y)',
 				'm-d-y'  => date_i18n( 'm-d-y' ) . ' (m-d-y)',
 			);
 
